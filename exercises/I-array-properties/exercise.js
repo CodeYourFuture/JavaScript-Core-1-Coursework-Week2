864896a44b6e15ab03166987888c55a087653d80
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-14 21:50:40 +0000

    Exercise I Completed

--- exercises/I-array-properties/exercise.js
+++ exercises/I-array-properties/exercise.js
@@ -1,15 +1,19 @@
 /*
-  Array properites
+  Array properties
   ----------------
   Complete the function to test if an array is empty (has no values in it)
 
 */
 
-function isEmpty(arr) {
-  return; // complete this statement
+function isEmpty(arr)
+{
+  if(arr.length > 0)
+    return false;
+  else
+    return true;
 }
 
-/* 
+/*
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
 var numbers = [1, 2, 3];
@@ -18,7 +22,7 @@ var names = [];
 console.log(isEmpty(numbers));
 console.log(isEmpty(names));
 
-/* 
+/*
   EXPECTED RESULT
   ---------------
   false
