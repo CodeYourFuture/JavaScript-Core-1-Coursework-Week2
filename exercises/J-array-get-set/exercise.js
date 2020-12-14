c4fae11f4e91d303092f0c579635d84447c5464c
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-14 21:53:18 +0000

    Exercise H Completed

--- exercises/J-array-get-set/exercise.js
+++ exercises/J-array-get-set/exercise.js
@@ -4,15 +4,17 @@
   Complete the functions below to get the first and last values from the array
 */
 
-function first(arr) {
-  return; // complete this statement
+function first(arr)
+{
+  return arr[0];
 }
 
-function last(arr) {
-  return; // complete this statement
+function last(arr)
+{
+  return arr[arr.length - 1];
 }
 
-/* 
+/*
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
 var numbers = [1, 2, 3];
@@ -22,7 +24,7 @@ console.log(first(numbers));
 console.log(last(numbers));
 console.log(last(names));
 
-/* 
+/*
   EXPECTED RESULT
   ---------------
   1
--- exercises/J-array-get-set/exercises2.js
+++ exercises/J-array-get-set/exercises2.js
@@ -7,13 +7,15 @@
 */
 
 var numbers = [1, 2, 3]; // Don't change this array literal declaration
+numbers.push(4);
+numbers[0] = 1;
 
-/* 
+/*
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
 console.log(numbers);
 
-/* 
+/*
   EXPECTED RESULT
   ---------------
   [1, 2, 3, 4]
