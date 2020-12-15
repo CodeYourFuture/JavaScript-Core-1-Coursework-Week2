ab6c265a89e5138e7f1de070d1f038b055e3cc40
Know-Thyself <67852769+Know-Thyself@users.noreply.github.com>
2020-12-15 17:47:43 +0000

    Array literals properties and declarations applied.

--- exercises/H-array-literals/exercise.js
+++ exercises/H-array-literals/exercise.js
@@ -5,7 +5,12 @@
 */
 
 var numbers = []; // add numbers from 1 to 10 into this array
-var mentors; // Create an array with the names of the mentors: Daniel, Irina and Rares
+var mentors = ["Daniel", "Irina", "Rares"];// Create an array with the names of the mentors: Daniel, Irina and Rares
+
+numbers.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//mentors.push("Daniel", "Irina", "Rares");
+
+
 
 /* 
   DO NOT EDIT BELOW THIS LINE
