49e9bd28d44bef9f2658054bdf08a02f431bb5ae
jip1984 <58789786+jip1984@users.noreply.github.com>
2020-12-16 17:01:56 +0000

    completed exercises

--- exercises/J-array-get-set/exercise.js
+++ exercises/J-array-get-set/exercise.js
@@ -5,11 +5,15 @@
 */
 
 function first(arr) {
-  return; // complete this statement
+  return arr[0]; // complete this statement
 }
 
 function last(arr) {
-  return; // complete this statement
+  return arr[2]; // complete this statement
+}
+
+function lastname(arr) {
+  return arr[3]; // complete this statement
 }
 
 /* 
