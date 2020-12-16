2873fbff88ba4bbebe09946916466dd006aed37a
abdiqaadir3110 <abdiqaadirabdillahi@gmail.com>
2020-12-16 16:05:34 +0000

    array get set
    second part of q1 still to do

--- exercises/J-array-get-set/exercises2.js
+++ exercises/J-array-get-set/exercises2.js
@@ -6,8 +6,11 @@
   - change the first value in the array to the number 1
 */
 
-var numbers = [1, 2, 3]; // Don't change this array literal declaration
+var numbers = [1, 2, 3];
+numbers.push(4); 
 
+numbers[0] === 1; // Don't change this array literal declaration
+// I THINK?
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
