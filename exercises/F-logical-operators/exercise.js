3da7cc686dccc39e0e99fad786799ab4e26666f9
alsafi25 <62480618+alsafi25@users.noreply.github.com>
2020-12-16 18:12:32 +0000

    F logical has completed

--- exercises/F-logical-operators/exercise.js
+++ exercises/F-logical-operators/exercise.js
@@ -11,15 +11,30 @@ var cssLevel = 4;
 
 // Finish the statement to check whether HTML, CSS knowledge are above 5
 // (hint: use the comparison operator from before)
-var htmlLevelAbove5;
-var cssLevelAbove5;
+var htmlLevelAbove5 = htmlLevel > 5;
+var cssLevelAbove5 = cssLevel > 5;
 
 // Finish the next two statement
 // Use the previous variables and logical operators
 // Do not "hardcode" the answers
 var cssAndHtmlAbove5;
 var cssOrHtmlAbove5;
+if (cssLevel > 5 && htmlLevel > 5) {
+    cssAndHtmlAbove5 = true;
 
+
+} else {
+    cssAndHtmlAbove5 = false;
+}
+
+
+if (cssLevel > 5 || htmlLevel > 5) {
+    cssOrHtmlAbove5 = true;
+
+
+} else {
+    cssOrHtmlAbove5 = false;
+}
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
