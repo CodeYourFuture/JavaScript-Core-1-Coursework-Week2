402e0f77ce5a9c3d24ac459144fe8b7cf63d5fb5
alsafi25 <62480618+alsafi25@users.noreply.github.com>
2020-12-16 18:17:22 +0000

    Creation function done

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -23,8 +23,10 @@ The function must NOT change the original array, arr.
 */
 
 function remove(arr, index) {
-  return; // complete this statement
+    return;
 }
+// complete this statement
+
 
 /*
 Write a function that:
@@ -34,54 +36,64 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(arr) {}
+function formatPercentage(arr) {
+
+    let array = [23, 18.103, 187.2, 0.372];
+    const st = array.map(function(s) {
+        return Math.round(s) + "%";
+
+    });
+    return st;
+
+}
+
+
+
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 const util = require("util");
 
 function test(test_name, actual, expected) {
-  let status;
+    let status;
 
-  let isEqual;
-  if (Array.isArray(expected)) {
-    isEqual = arraysEqual(actual, expected);
-  } else {
-    isEqual = actual === expected;
-  }
+    let isEqual;
+    if (Array.isArray(expected)) {
+        isEqual = arraysEqual(actual, expected);
+    } else {
+        isEqual = actual === expected;
+    }
 
-  if (isEqual) {
-    status = "PASSED";
-  } else {
-    status = `FAILED: expected: ${util.inspect(
+    if (isEqual) {
+        status = "PASSED";
+    } else {
+        status = `FAILED: expected: ${util.inspect(
       expected
     )} but your function returned: ${util.inspect(actual)}`;
-  }
+    }
 
-  console.log(`${test_name}: ${status}`);
+    console.log(`${test_name}: ${status}`);
 }
 
 function arraysEqual(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length != b.length) return false;
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
 
-  for (let i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
-  }
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
 
-  return true;
+    return true;
 }
 
 test(
-  "tidyUpString function works - case 1",
-  tidyUpString(["/Daniel ", "irina ", " Gordon", "ashleigh "]),
-  ["daniel", "irina", "gordon", "ashleigh"]
+    "tidyUpString function works - case 1",
+    tidyUpString(["/Daniel ", "irina ", " Gordon", "ashleigh "]), ["daniel", "irina", "gordon", "ashleigh"]
 );
 test(
-  "tidyUpString function works - case 2",
-  tidyUpString([" /Sanyia ", " Michael ", "AnTHonY ", "   Tim   "]),
-  ["sanyia", "michael", "anthony", "tim"]
+    "tidyUpString function works - case 2",
+    tidyUpString([" /Sanyia ", " Michael ", "AnTHonY ", "   Tim   "]), ["sanyia", "michael", "anthony", "tim"]
 );
 
 test("validate function works - case 1", validate(10), true);
@@ -91,19 +103,17 @@ test("validate function works - case 4", validate("Ten"), false);
 test("validate function works - case 5", validate(108), false);
 
 test("remove function works - case 1", remove([10, 293, 292, 176, 29], 3), [
-  10,
-  293,
-  292,
-  29,
+    10,
+    293,
+    292,
+    29,
 ]);
 test(
-  "remove function works - case 2",
-  remove(["a", "b", "c", "d", "e", "f", "g"], 6),
-  ["a", "b", "c", "d", "e", "f"]
+    "remove function works - case 2",
+    remove(["a", "b", "c", "d", "e", "f", "g"], 6), ["a", "b", "c", "d", "e", "f"]
 );
 
 test(
-  "formatPercentage function works - case 1",
-  formatPercentage([23, 18.103, 187.2, 0.372]),
-  ["23%", "18.1%", "100%", "0.37%"]
+    "formatPercentage function works - case 1",
+    formatPercentage([23, 18.103, 187.2, 0.372]), ["23%", "18.1%", "100%", "0.37%"]
 );
