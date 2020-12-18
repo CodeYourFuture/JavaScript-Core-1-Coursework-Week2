d079e72e47e96bc51a284585e14a4a8912a2d8e1
Kos0606 <65927936+Kos0606@users.noreply.github.com>
2020-12-18 20:41:59 +0000

    JavaScript-Core-1-Coursework-week2

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -8,11 +8,12 @@
 
   1. This program throws an error. Why? (If you can't find it, try executing it).
   2. Remove the line that throws the error.
-  3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
-  7. What is the value of the "a" outer variable when "f1" is called for the first time?
+  console.log(b); this code must be removed as b is undefined
+  3. What is printed to the console? 2 6 4 9 6 13 8
+  4. How many times is "f1" called? 2 times
+  5. How many times is "f2" called? 3 times
+  6. What value does the "a" parameter take in the first "f1" call? 6
+  7. What is the value of the "a" outer variable when "f1" is called for the first time? 8
 */
 
 let x = 2;
@@ -26,9 +27,9 @@ const f2 = function (a, b) {
   return a + b + x;
 };
 
-console.log(x);
-console.log(a);
-console.log(b);
+console.log(x);  // output is 2
+console.log(a);  // output is 6
+console.log(b);  // undefined
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
