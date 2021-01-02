d317e696415051ac1af5306ac53d7005a663f961
haroon iqbal <h.iqbal0121@gmail.com>
2021-01-02 11:23:45 +0000

    mandatory 2 complete

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -5,7 +5,15 @@ Write a function that:
 - removes any forward slashes (/) in the strings
 - makes the string all lowercase
 */
-function tidyUpString(strArr) {}
+function tidyUpString(strArr) {
+
+  let edited = strArr;
+	for (var i=0; i<edited.length; i++) {
+		edited[i] = edited[i].trim().replace(/\//g, "").toLowerCase();
+	}
+	return edited;
+
+}
 
 /*
 Complete the function to check if the variable `num` satisfies the following requirements:
@@ -15,7 +23,13 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if ((num % 2 === 0) && (typeof num === "number") && (num <= 100)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /* 
 Write a function that returns a copy of the given array arr, but with the element at the given index, index removed.
@@ -23,7 +37,10 @@ The function must NOT change the original array, arr.
 */
 
 function remove(arr, index) {
-  return; // complete this statement
+  
+  arr.splice(index, 1);
+  return arr;  // complete this statement
+
 }
 
 /*
@@ -34,7 +51,19 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(arr) {}
+function formatPercentage(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] <= 100) {
+      arr[i] = Math.round((arr[i] + Number.EPSILON) * 100) / 100 + "%"; 
+    } else {
+      arr[i] = "100%"; 
+    }
+  }
+  return arr;
+}
+
+// question suggests using '.toFixed(2)' but fails test
+ 
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
