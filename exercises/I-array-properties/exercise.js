d095a5f2bf263ac3e9e71262a4aac465d1a88823
RodrigueNdzana <54138415+RodrigueNdzana@users.noreply.github.com>
2021-01-23 22:13:51 +0200

    I-array-properties
    done

--- exercises/I-array-properties/exercise.js
+++ exercises/I-array-properties/exercise.js
@@ -6,7 +6,12 @@
 */
 
 function isEmpty(arr) {
-  return; // complete this statement
+  if (arr.length === 0){   // this check if there no items in the array by targeting their length. so if the length is not equal to 0, then there are items in the array
+    return true; // complete this statement
+  }
+  else{
+    return false;
+  }
 }
 
 /* 
