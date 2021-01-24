74df333121da15bad02f41f25171f2fb97214f7b
askinekinci <askinekincihf@gmail.com>
2021-01-24 21:39:54 +0000

    completed F-logical-operators

--- exercises/F-logical-operators/exercise2.js
+++ exercises/F-logical-operators/exercise2.js
@@ -5,7 +5,21 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
+// function isNegative(num) {
+//   if (num < 0){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+const isNegative = num => (num < 0) ? true : false;
+
+const isBetween5and10 = num => (num >=5 && num <= 10) ? true : false;
+
+const isShortName = name => (name.length < 10) ? true : false;
+
+const startsWithD = name => (name[0] === "D") ? true : false;
 
 /* 
   DO NOT EDIT BELOW THIS LINE
@@ -24,5 +38,5 @@ console.log("Does Daniel start with 'D'?", startsWithD("Daniel"));
   Is 5 a negative number? false
   Is 10 in the range 5-10? true
   Is Daniel a short name? true
-  Does Daniel start with 'D'?
+  Does Daniel start with 'D'? true
 */
