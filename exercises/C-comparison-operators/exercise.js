1770d9c6dcd40b33351b0742199e9baf9d1c3396
Kudah Shambare <Kudahshambare263@gmail.com>
2021-01-25 00:49:55 +0200

    Exercises Completed

--- exercises/C-comparison-operators/exercise.js
+++ exercises/C-comparison-operators/exercise.js
@@ -7,14 +7,15 @@
 
 var studentCount = 16;
 var mentorCount = 9;
-var moreStudentsThanMentors; // finish this statement
+var moreStudentsThanMentors=studentCount>mentorCount; // finish this statement
 
 var roomMaxCapacity = 25;
-var enoughSpaceInRoom; // finish this statement
+var enoughSpaceInRoom=roomMaxCapacity>=studentCount+mentorCount; // finish this statement
 
 var personA = "Daniel";
 var personB = "Irina";
-var sameName; // finish this statement
+let same=personA.localeCompare(personB);
+var sameName= same===0// finish this statement
 
 /* 
   DO NOT EDIT BELOW THIS LINE
--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -8,6 +8,12 @@
 
 var name = "Daniel";
 var danielsRole = "mentor";
+if (danielsRole.localeCompare("mentor")===0){
+  console.log( "Hi, I'm Daniel, I'm a mentor.");
+}
+if(danielsRole.localeCompare("student")===0){
+  console.log("Hi, I'm Daniel, I'm a student.");
+}
 
 /* 
 EXPECTED RESULT
