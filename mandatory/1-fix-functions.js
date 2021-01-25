712bdc88e2357fd8ccd4a7f8e7db82bd7835f5e3
alinosratipour <74776647+alinosratipour@users.noreply.github.com>
2021-01-25 15:39:58 +0000

    fix functions ishappy task completed

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -9,45 +9,45 @@
   NOTE:Only make edits inside the function
 
 */
+// 
+ let isHappy = true;
 
-function mood() {
-  let isHappy = true;
-
-  if (isHappy) {
-    return "I am happy";
-  } else {
-    return "I am not happy";
-  }
+function mood(isHappy) {
+    if (isHappy) {
+      return "I am happy";
+    } else if(!isHappy) {
+      return "I am not happy";
+    }
 }
 
-function greaterThan10(num) {
-  let isBigEnough;
+// function greaterThan10(num) {
+//   let isBigEnough;
 
-  if (isBigEnough) {
-    return "num is greater than 10";
-  } else {
-    return "num is not big enough";
-  }
-}
+//   if (isBigEnough) {
+//     return "num is greater than 10";
+//   } else {
+//     return "num is not big enough";
+//   }
+// }
 
-function sortArray(letters) {
-  let sortedLetters = letters;
+// function sortArray(letters) {
+//   let sortedLetters = letters;
 
-  return sortedLetters;
-}
+//   return sortedLetters;
+// }
 
-function first5(numbers) {
-  let sliced;
+// function first5(numbers) {
+//   let sliced;
 
-  return sliced;
-}
+//   return sliced;
+// }
 
-function get3rdIndex(arr) {
-  let index = 3;
-  let element;
+// function get3rdIndex(arr) {
+//   let index = 3;
+//   let element;
 
-  return element;
-}
+//   return element;
+// }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -88,43 +88,43 @@ function arraysEqual(a, b) {
 
 test("mood function works for true", mood(true), "I am happy");
 test("mood function works for false", mood(false), "I am not happy");
-test(
-  "greaterThanTen function works for 11",
-  greaterThan10(11),
-  "num is greater than 10"
-);
-test(
-  "greaterThanTen function works for 10",
-  greaterThan10(10),
-  "num is not big enough"
-);
-test(
-  "greaterThanTen function works for 9",
-  greaterThan10(9),
-  "num is not big enough"
-);
-test("sortArray function works", sortArray(["a", "n", "c", "e", "z", "f"]), [
-  "a",
-  "c",
-  "e",
-  "f",
-  "n",
-  "z",
-]);
+// test(
+//   "greaterThanTen function works for 11",
+//   greaterThan10(11),
+//   "num is greater than 10"
+// );
+// test(
+//   "greaterThanTen function works for 10",
+//   greaterThan10(10),
+//   "num is not big enough"
+// );
+// test(
+//   "greaterThanTen function works for 9",
+//   greaterThan10(9),
+//   "num is not big enough"
+// );
+// test("sortArray function works", sortArray(["a", "n", "c", "e", "z", "f"]), [
+//   "a",
+//   "c",
+//   "e",
+//   "f",
+//   "n",
+//   "z",
+// ]);
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-test("first5 function works", first5(numbers), [1, 2, 3, 4, 5]);
-if (!arraysEqual(numbers, [1, 2, 3, 4, 5, 6, 7, 8])) {
-  console.log("PROBLEM: first5 changed its input array - it shouldn't!");
-}
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// test("first5 function works", first5(numbers), [1, 2, 3, 4, 5]);
+// if (!arraysEqual(numbers, [1, 2, 3, 4, 5, 6, 7, 8])) {
+//   console.log("PROBLEM: first5 changed its input array - it shouldn't!");
+// }
 
-test(
-  "get3rdIndex function works - case 1",
-  get3rdIndex(["fruit", "banana", "apple", "strawberry", "raspberry"]),
-  "strawberry"
-);
-test(
-  "get3rdIndex function works - case 2",
-  get3rdIndex([11, 37, 62, 18, 19, 3, 30]),
-  18
-);
+// test(
+//   "get3rdIndex function works - case 1",
+//   get3rdIndex(["fruit", "banana", "apple", "strawberry", "raspberry"]),
+//   "strawberry"
+// );
+// test(
+//   "get3rdIndex function works - case 2",
+//   get3rdIndex([11, 37, 62, 18, 19, 3, 30]),
+//   18
+// );
