30d80b11fda32d5f98ef2bff66971cfade6c9cd8
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-01-25 16:02:23 +0200

    Did the mandatory folder
    Did the 3 exercieses in the mandatory folder

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -5,7 +5,16 @@ Write a function that:
 - removes any forward slashes (/) in the strings
 - makes the string all lowercase
 */
-function tidyUpString(strArr) {}
+function tidyUpString(strArr) {
+  for (let x = 0; x < strArr.length; x++) {
+    let noSpaces = strArr[x].trim(); // trims the string and removes 'whitespaces' from each end of the string
+    let noFwdSlash = noSpaces.replace("/", '') // removes the forward slashes
+    let lowerCase = noFwdSlash.toLowerCase(); // makes the string all lowercase
+    strArr[x]=lowerCase;
+  }
+  return strArr
+    //.trim toLowerCase (str.replace("/", '')) -> thought process;
+}
 
 /*
 Complete the function to check if the variable `num` satisfies the following requirements:
@@ -15,7 +24,17 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+const numberEvenLessEqual100 = true
+const notNumberUnevenMore100 = false
+
+function validate(num) {
+  if (typeof num == "number" && num % 2 == 0 && num <= 100) {
+    return numberEvenLessEqual100;
+  }
+  else {
+    return notNumberUnevenMore100
+  }
+}
 
 /* 
 Write a function that returns a copy of the given array arr, but with the element at the given index, index removed.
@@ -23,7 +42,8 @@ The function must NOT change the original array, arr.
 */
 
 function remove(arr, index) {
-  return; // complete this statement
+  arr.splice(index, 1)
+  return arr; // complete this statement
 }
 
 /*
@@ -34,8 +54,27 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(arr) {}
+function formatPercentage(arr) {
+  for (let z = 0; z < arr.length; z++){
+    
+  // checking for values over 100 and replacing it with 100
+    let valueMoreThan100 = arr[z];
+    if ( valueMoreThan100 > 100) {
+      valueMoreThan100 = 100
+    }
+  // round the value off to two decimals
+    else{
+      valueMoreThan100 = Math.round(arr[z] * 100) / 100;
+    }
+    // format the values as percentages so 10 would be 10% and etc.
+    let twoDecimals = valueMoreThan100;
+    let percentValues = twoDecimals + "%";
+    arr[z] = percentValues;
+  }
+  return arr;
+}
 
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 const util = require("util");
