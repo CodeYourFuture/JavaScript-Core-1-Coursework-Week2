c2630b7252abf30fc73e1748a529955edc9e0cd5
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2021-01-25 16:47:16 +0000

    finishing mandatory part except formatPercentage( ) function exercise

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -5,7 +5,14 @@ Write a function that:
 - removes any forward slashes (/) in the strings
 - makes the string all lowercase
 */
-function tidyUpString(strArr) {}
+function tidyUpString(strArr) {
+  for (let i = 0; i < strArr.length; i++) {
+    strArr[i].trim();
+    strArr[i].replace(/\//g, "");
+    strArr[i].toLowerCase();
+  }
+  return strArr;
+}
 
 /*
 Complete the function to check if the variable `num` satisfies the following requirements:
@@ -15,7 +22,11 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === "number" && num % 2 === 0 && num <= 100) {
+    return true;
+  } else return false;
+}
 
 /* 
 Write a function that returns a copy of the given array arr, but with the element at the given index, index removed.
@@ -23,7 +34,9 @@ The function must NOT change the original array, arr.
 */
 
 function remove(arr, index) {
-  return; // complete this statement
+  let newArray = arr.slice();
+  newArray.splice(index, 1);
+  return newArray;
 }
 
 /*
@@ -34,7 +47,15 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(arr) {}
+function formatPercentage(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 100) {
+      arr[i] = "100%";
+      console.log(arr[i]);
+    } else arr[i] = `${arr[i].toFixed(2)}%`;
+  }
+  return arr;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
