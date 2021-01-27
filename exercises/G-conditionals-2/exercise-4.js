dd6c81909f2d52b514075b9ba2f785734ed826cb
ASLANDENIZ <36959348+ASLANDENIZ@users.noreply.github.com>
2021-01-27 02:19:22 +0000

    exercises done

--- exercises/G-conditionals-2/exercise-4.js
+++ exercises/G-conditionals-2/exercise-4.js
@@ -8,7 +8,14 @@
   Hint: Google how to check if a string contains a word
 */
 
-function containsCode(sentence) {}
+function containsCode(sentence) {
+  let codeIncluded = sentence.includes("code");
+  if (codeIncluded === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /* 
 DO NOT EDIT BELOW THIS LINE
--- exercises/J-array-get-set/exercise.js
+++ exercises/J-array-get-set/exercise.js
@@ -5,11 +5,12 @@
 */
 
 function first(arr) {
-  return; // complete this statement
+  return arr[0]; // complete this statement ; 
 }
 
 function last(arr) {
-  return; // complete this statement
+  let arrayLength = arr.length - 1;
+  return arr[arrayLength];
 }
 
 /* 
