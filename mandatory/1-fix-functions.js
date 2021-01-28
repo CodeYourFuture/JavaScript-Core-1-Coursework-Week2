5ddf5110597845e7e82d88bc1420b15a4e35241c
ademalagoz <65690057+ademalagoz@users.noreply.github.com>
2021-01-28 02:29:01 +0000

    Mandatory 1-2

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,10 +10,10 @@
 
 */
 
-function mood() {
-  let isHappy = true;
+function mood(isHappy) {
+  // let isHappy = true;
 
-  if (isHappy) {
+  if (isHappy === true) {
     return "I am happy";
   } else {
     return "I am not happy";
@@ -21,9 +21,9 @@ function mood() {
 }
 
 function greaterThan10(num) {
-  let isBigEnough;
+  let isBigEnough = 10;
 
-  if (isBigEnough) {
+  if (num > isBigEnough) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
@@ -31,20 +31,20 @@ function greaterThan10(num) {
 }
 
 function sortArray(letters) {
-  let sortedLetters = letters;
+  let sortedLetters = letters.sort();
 
   return sortedLetters;
 }
 
 function first5(numbers) {
-  let sliced;
+  let sliced = numbers.slice(0,5);
 
   return sliced;
 }
 
 function get3rdIndex(arr) {
   let index = 3;
-  let element;
+  let element = arr[index];
 
   return element;
 }
--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -5,7 +5,13 @@ Write a function that:
 - removes any forward slashes (/) in the strings
 - makes the string all lowercase
 */
-function tidyUpString(strArr) {}
+function tidyUpString(strArr) {
+  for(let i =0; i < strArr.length; i++){
+    strArr[i] = strArr[i].trim().replace('/','').toLowerCase();
+
+  }
+return strArr;
+}
 
 /*
 Complete the function to check if the variable `num` satisfies the following requirements:
@@ -15,7 +21,16 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if(typeof num === "number" && num %2 === 0 && num <= 100){
+    return true;
+  }
+  else {
+  return false;
+  }
+  
+}
+
 
 /* 
 Write a function that returns a copy of the given array arr, but with the element at the given index, index removed.
@@ -23,7 +38,10 @@ The function must NOT change the original array, arr.
 */
 
 function remove(arr, index) {
-  return; // complete this statement
+
+  let copyArray = arr;
+  copyArray.splice(index , 1);
+  return copyArray; // complete this statement
 }
 
 /*
@@ -34,7 +52,24 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(arr) {}
+function formatPercentage(arr) {
+ for(let i =0; i < arr.length; i++) {
+   if(arr[i] >100){arr[i] =100;} 
+   arr[i] = +arr[i].toFixed(2);
+   arr[i] = arr[i] +'%';
+    }
+   return arr
+   }
+
+//  function formatPercentage(arr) {
+//   for (i=0;i<arr.length;i++){
+//     if (arr[i]>100) {arr[i]=100;}
+//     arr[i] = +arr[i].toFixed(2);
+//     arr[i] = `${arr[i]}%`;
+//   }
+
+//     return arr;
+// }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
