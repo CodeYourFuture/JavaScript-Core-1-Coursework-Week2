6d9d610193539b1a4ce96a7265a5932a3a3d104c
askinekinci <askinekincihf@gmail.com>
2021-01-29 19:22:30 +0000

    modified D--predicates

--- exercises/D-predicates/exercise.js
+++ exercises/D-predicates/exercise.js
@@ -7,22 +7,13 @@
 
 // Finish the predicate function to test if the passed number is negative (less than zero)
 // function isNegative(number) {
-//   if(number < 0){
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+//   if number < 0;
 
 const isNegative = number => (number < 0) ? true : false; 
 
 // Finish the predicate function to test if the passed number is between 0 and 10
 // function isBetweenZeroAnd10(number) {
-//   if (number >= 0 && number <= 10){
-//     return true;
-//   } else {
-//     return false;
-//   }
+//   return number >= 0 && number <= 10;
 // }
 
 const isBetweenZeroAnd10 = number => (number >= 0 && number <= 10) ? true : false;
