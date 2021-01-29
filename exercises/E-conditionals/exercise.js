a65f124de8c774a14c8851abbc8bfcaf4d7d2bc3
Samuelkau <72687211+Samuelkau@users.noreply.github.com>
2021-01-29 15:47:40 +0200

    Exercise E

--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -9,6 +9,10 @@
 var name = "Daniel";
 var danielsRole = "mentor";
 
+if (danielsRole ) {
+  console.log ("Hi, I'm Daniel, I'm a mentor.");
+}
+else console.log ("Hi, I'm Daniel, I'm a student.");
 /* 
 EXPECTED RESULT
 ---------------
