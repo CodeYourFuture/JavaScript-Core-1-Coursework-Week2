b35c4ea4c372f6f9491bbc7667082284a414bc15
Domenico <domenico.cyf@gmail.com>
2021-01-29 23:36:01 +0000

    Finished all the exercises

--- exercises/I-array-properties/exercise.js
+++ exercises/I-array-properties/exercise.js
@@ -6,8 +6,12 @@
 */
 
 function isEmpty(arr) {
-  return; // complete this statement
-}
+   if (arr.length === 0) {
+     return true;
+    }; // complete this statement
+     return false;
+    
+  }
 
 /* 
   DO NOT EDIT BELOW THIS LINE
