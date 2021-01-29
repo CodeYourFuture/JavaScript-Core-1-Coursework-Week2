1da1cbb877793ca6afc2d0cbdce8cdea2e3f14e2
mulindierick <37551785+mulindierick@users.noreply.github.com>
2021-01-29 05:06:09 +0300

    updated 3-playing-computer
    updated 3-playing-computer

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -9,16 +9,7 @@
   1. This program throws an error. Why? (If you can't find it, try executing it). || It throws a reference error 
   becuase "b" is not defined
   2. Remove the line that throws the error.
-  3. What is printed to the console? || 
-2
-6
-5
-4
-9
-6
-13
-8
-
+  3. What is printed to the console? || values of x, a d, and e
   4. How many times is "f1" called?  ||  3
   5. How many times is "f2" called?  ||  2
   6. What value does the "a" parameter take in the first "f1" call? || 8
@@ -27,8 +18,6 @@
 
 let x = 2;
 let a = 6;
-let b = 3;
-
 
 const f1 = function (a, b) {
   return a + b;
@@ -42,14 +31,19 @@ const f2 = function (a, b) {
 
 console.log(x); 
 console.log(a);
-console.log(b); 
+// console.log(b); 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
   if (i % 2 === 0) {
     const d = f2(i, x); 
-    console.log(d); // 0, 2, 4
+    console.log(i, x);
+    console.log(d); 
   } else {
     const e = f1(i, a);
-    console.log(e); // 1, 3
+    console.log(i, a)
+    console.log(e); 
   }
 }
+
+
+
