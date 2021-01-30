39d056d088c90cd202038a689b56392be0bffc01
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-01-30 02:04:13 +0000

    solved extra exercise
    there is a bug in test function so does not pass every test

--- extra/1-radio-stations.js
+++ extra/1-radio-stations.js
@@ -14,6 +14,13 @@
  */
 
 // `getAllFrequencies` goes here
+const getAllFrequencies = () => {
+  var arr = [];
+  for (var i = 87; i <= 108; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
 
 /**
  * Next, let's write a function that gives us only the frequencies that are radio stations.
@@ -21,10 +28,15 @@
  *
  * This function should:
  * - Get the available frequencies from `getAllFrequencies`
- * - There is a helper function called isRadioFrequency that takes an integer as an argument and returns a boolean.
+ * - There is a helper function called isRadioStation that takes an integer as an argument and returns a boolean.
  * - Return only the frequencies that are radio stations.
  */
 // `getStations` goes here
+
+//ToDo: TestBug; every isRadioStation function call check different random array So sometimes failed
+const getStations = ()=>{
+  return getAllFrequencies().filter((value) => isRadioStation(value) === true);
+}
 
 /* ======= TESTS - DO NOT MODIFY ======= */
 
