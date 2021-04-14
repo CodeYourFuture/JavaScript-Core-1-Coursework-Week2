ff10a7f32c68e70d9625beb0dcb788f5747735f2
Sally McGrath <sally@supercooldesign.co.uk>
2021-04-14 19:51:47 +0100

    fix predicates test
    and swap order of exercise so predicates come after logical operators
    resolves CodeYourFuture/syllabus#233

--- exercises/F-predicates/exercise.js
+++ exercises/F-predicates/exercise.js
@@ -1,7 +1,7 @@
 /*
   Predicates
   ---------------------------------
-  Write a predicate to predicates
+  Write two predicate functions
   The variables should have values that match the expected results.
 */
 
@@ -14,17 +14,17 @@ function isBetweenZeroAnd10(number) {}
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
-var number = 5;
-var numberNegative = isNegative(number);
-var numberBetweenZeroAnd10 = isBetweenZeroAnd10(number);
-console.log("The number in test is " + number);
-console.log("Is the number negative? " + numberNegative);
-console.log("Is the number between 0 and 10? " + numberBetweenZeroAnd10);
+
+console.log(`Is 5 negative? ${isNegative(5)}`);
+console.log(`Is -5 negative? ${isNegative(-5)}`);
+console.log(`Is 5 between 0 and 10? ${isBetweenZeroAnd10(5)}`);
+console.log(`Is -5 between 0 and 10? ${isBetweenZeroAnd10(-5)}`);
 
 /* 
   EXPECTED RESULT
   ---------------
-  The number in test is 5
-  Is the number negative? false
-  Is the number between 0 and 10? true
+  1. Is 5 negative? false 
+  2. Is -5 negative? true 
+  3. Is 5 between 0 and 10? true 
+  5. Is -5 between 0 and 10? false 
 */
