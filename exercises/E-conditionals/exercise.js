84f749a75586b8b5a94baedaa6b1e83d779d6182
Mya <myaskylar14@gmail.com>
2021-06-27 19:37:49 +0100

    Done exercise E & F

--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -9,6 +9,16 @@
 var name = "Daniel";
 var danielsRole = "mentor";
 
+function createGreetingForDaniel(role) {
+  if (role === danielsRole ) {
+    return `Hi, I'm ${name}, I'm a ${danielsRole}`;
+  } else {
+    return `Hi, I'm ${name}, I'm a student`;
+  }
+}
+
+console.log(createGreetingForDaniel(danielsRole));
+
 /* 
 EXPECTED RESULT
 ---------------
