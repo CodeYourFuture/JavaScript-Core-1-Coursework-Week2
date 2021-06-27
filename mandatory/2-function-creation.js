10baffc1ef3c26a9630d0a729ec9f6062fb4781a
Mya <myaskylar14@gmail.com>
2021-06-27 22:47:19 +0100

    Update 2-function-creation.js

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,13 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (num % 2 == 0 && num <= 100 && typeof(num) === "number"){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /*
 Write a function that:
@@ -16,7 +22,16 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+  if (num > 100){
+    let number = 100;
+    return `${number}%`;
+  } else {
+    let roundedNum = Math.round(num * 100) / 100;
+    return `${roundedNum}%`
+  }
+}
+
 
 /*
 Write a function that:
