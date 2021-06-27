f5b08a839498d687b73656e4797ab10065144602
Anza-Azam <anzaazam.nw4@gmail.com>
2021-06-27 09:21:49 -0700

    E-conditionals-done

--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -1,13 +1,20 @@
-/*
-  Conditionals
-  ---------------------------------
-  Add an if statement to check Daniel's role in a CYF class.
-  If Daniel is a mentor, print out "Hi, I'm Daniel, I'm a mentor."
-  If Daniel is a student, print out "Hi, I'm Daniel, I'm a student."
-*/
+/**
+ * /*
+ *   Conditionals
+ *   ---------------------------------
+ *   Add an if statement to check Daniel's role in a CYF class.
+ *   If Daniel is a mentor, print out "Hi, I'm Daniel, I'm a mentor."
+ *   If Daniel is a student, print out "Hi, I'm Daniel, I'm a student."
+ *
+ * @format
+ */
 
 var name = "Daniel";
 var danielsRole = "mentor";
+if (danielsRole === "mentor")
+	console.log(`Hi, I'm Daniel, I'm a ${danielsRole}.`);
+if (danielsRole === "student")
+	console.log(`Hi, I'm Daniel, I'm a ${danielsRole}.`);
 
 /* 
 EXPECTED RESULT
