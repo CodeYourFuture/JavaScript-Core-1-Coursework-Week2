08ae70287975fa3bdf3fe6e221999f1743e169db
Anza-Azam <anzaazam.nw4@gmail.com>
2021-06-27 17:49:57 -0700

    3-playing-computer.js update/answered questions

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -1,42 +1,67 @@
-/*
-  You have to predict the output of this program WITHOUT EXECUTING IT.
-
-  In order to do this, try writing down the value that all variables take
-  during each step of the program execution.
-
-  Answer the following questions:
-
-  1. This program throws an error. Why? (If you can't find it, try executing it).
-  2. Remove the line that throws the error.
-  3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
-  7. What is the value of the "a" outer variable when "f1" is called for the first time?
-*/
+/**
+ * /*
+ *   You have to predict the output of this program WITHOUT EXECUTING IT.
+ *
+ *   In order to do this, try writing down the value that all variables take
+ *   during each step of the program execution.
+ *
+ *   Answer the following questions:
+ *
+ *   1. This program throws an error. Why? (If you can't find it, try executing it).
+ * 
+ *   This program will throw an error because b has not been declared.
+ * 
+ *   2. Remove the line that throws the error.
+ * 
+ *   //committed out console.log(b);
+ * 
+ *   3. What is printed to the console?
+ * 
+ *   2
+ *   6
+ *   4
+ *   9
+ *   6
+ *   13
+ *   8
+ * 
+ *   4. How many times is "f1" called?
+ *   2 times
+ *   5. How many times is "f2" called?
+ *   3 times
+ *   6. What value does the "a" parameter take in the first "f1" call?
+ * 
+ *   "a" parameter value will be 1 because it is i in f1 first call
+ * 
+ *   7. What is the value of the "a" outer variable when "f1" is called for the first time?
+ * 
+ *   The value of a is 8
+ *
+ * @format
+ */
 
 let x = 2;
 let a = 6;
 
 const f1 = function (a, b) {
-  return a + b;
+	return a + b;
 };
 
 const f2 = function (a, b) {
-  return a + b + x;
+	return a + b + x;
 };
 
 console.log(x);
 console.log(a);
-console.log(b);
+//console.log(b);
 
 for (let i = 0; i < 5; ++i) {
-  a = a + 1;
-  if (i % 2 === 0) {
-    const d = f2(i, x);
-    console.log(d);
-  } else {
-    const e = f1(i, a);
-    console.log(e);
-  }
+	a = a + 1;
+	if (i % 2 === 0) {
+		const d = f2(i, x);
+		console.log(d);
+	} else {
+		const e = f1(i, a);
+		console.log(e);
+	}
 }
