a96e423a2c8d366bf75ea09b6e63b5b560005b5e
Sharm-Dev <sharmainetaylor@ymail.com>
2021-06-28 22:49:49 +0100

    Completed questions

--- exercises/G-conditionals-2/exercise-4.js
+++ exercises/G-conditionals-2/exercise-4.js
@@ -8,14 +8,20 @@
   Hint: Google how to check if a string contains a word
 */
 
-function containsCode(sentence) {}
+function containsCode(sentence) {
+  if (sentence.indexOf("code") >= 0) {
+    return true;
+  } else if (sentence2, sentence3) {
+    return false;
+  }
+}
 
 /* 
 DO NOT EDIT BELOW THIS LINE
 --------------------------- */
-var sentence1 = "code your future";
-var sentence2 = "draw your future";
-var sentence3 = "design your future";
+const sentence1 = "code your future";
+const sentence2 = "draw your future";
+const sentence3 = "design your future";
 
 console.log("'" + sentence1 + "': " + containsCode(sentence1));
 console.log("'" + sentence2 + "': " + containsCode(sentence2));
