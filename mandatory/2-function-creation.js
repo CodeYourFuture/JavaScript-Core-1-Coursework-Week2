3e035a458386db873ea6c59a279143eb5499e901
humailhasankhan <h.hasan.khan88@hotmail.com>
2021-06-29 19:54:45 +0100

    Finished mandatory 1 and 2 and passed all tests

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,13 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === "number" && num % 2 === 0 && num <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /*
 Write a function that:
@@ -16,8 +22,18 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
-
+function formatPercentage(num) {
+  let convertToPercentage = `${num}%`;
+  if (num % 1 === 0 && num <= 100) {
+    return convertToPercentage;
+  } else if (num > 100) {
+    return (convertToPercentage = `${Math.max(100)}%`);
+  } else if (num % 1 !== 0 && num > 1) {
+    return (convertToPercentage = `${num.toFixed(1)}%`);
+  } else if (num % 1 !== 0 && num < 1) {
+    return (convertToPercentage = `${num.toFixed(2)}%`);
+  }
+}
 /*
 Write a function that:
 - takes an array of strings as input
@@ -25,7 +41,18 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+
+function tidyUpStrings(arrayOfStrings) {
+  const regex = /^.+\s$/g;
+  let tidyArray = [];
+  for (let i = 0; i < arrayOfStrings.length; i++) {
+    let lowerCaseStr = arrayOfStrings[i].toLowerCase();
+    let removeSpace = lowerCaseStr.trim();
+    let removeForwardSlash = removeSpace.replace("/", "");
+    tidyArray.push(removeForwardSlash);
+  }
+  return tidyArray;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -77,7 +104,7 @@ test("tidyUpString function works", () => {
       "ashleigh   ",
       "   Alastair  ",
       " anne marie  ",
-    ])  
+    ])
   ).toEqual([
     "daniel",
     "sanyia",
@@ -87,5 +114,5 @@ test("tidyUpString function works", () => {
     "ashleigh",
     "alastair",
     "anne marie",
-  ]); 
+  ]);
 });
