225ca6284bdb15a0249009eff1b491e6e6d4c805
Amanda Obiajulu Nwadukwe <xtremeli.basic@gmail.com>
2021-06-30 17:57:34 +0100

    Mandatory completed
    Amanda has completed all the exercises in the mandatory folder

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -28,7 +28,23 @@ const f2 = function (a, b) {
 
 console.log(x);
 console.log(a);
-console.log(b);
+/* 
+Answers:
+
+1. There will be an error here because b is not defined
+2. I removed the line: console.log(b);
+3. 2   -  console.log(x)
+   6   -   console.log(a)
+   4   -  when i = 0, using f2(i,x): a = 7 and x = 2 so i + x + x = 0 + 2 + 2 = 4
+   9   -  when i = 1, using f1(i,a): a = 8 and x = 2 so i + a = 1 + 8 = 9
+   6   -  when i = 2, using f2(i,x): a = 9 and x = 2 so i + x + x = 2 + 2 + 2 = 6
+   13  -  when i = 3, using f1(i,a): a = 10 and x = 2 so i + a = 3 + 10 = 13
+   8   -  when i = 4, using f2(i,x): a = 11 and x = 2 so i + x + x = 4 + 2 + 2 = 8
+4. f1 is called 2 times
+5. f2 is called 3 times
+6. In the first f1 call a is 8
+7. [The first value of the a outer variable when f1 is called for the first time is 8
+*/
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
