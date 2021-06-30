2a37fdc1c1f56a4ae83fec294301b33c03cf1258
Gulnihal <gh.naldoken@gmail.com>
2021-06-30 13:43:55 +0100

    corrections made on exe3

--- exercises/G-conditionals-2/exercise-3.js
+++ exercises/G-conditionals-2/exercise-3.js
@@ -9,9 +9,9 @@
 */
 
 function calculateGrade(mark) {
-  if (mark > 80){
+  if (mark >= 80){
     return "A";
-  }else if(mark <= 80 && mark > 60) {
+  }else if(mark < 80 && mark > 60) {
     return "B";
   }else if(mark <= 60 && mark>= 50){
     return "C";
