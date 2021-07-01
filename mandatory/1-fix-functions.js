d9f7a935752f24e628cb4ca275a3a9bb7d86142a
Niamh Madden <niamhmadden15@gmail.com>
2021-07-01 21:37:07 +0100

    Completed functions in fix functions

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,10 +10,8 @@
 
 */
 
-function mood() {
-  let isHappy = true;
-
-  if (isHappy) {
+function mood(myMood) {
+  if (myMood === isHappy) {
     return "I am happy";
   } else {
     return "I am not happy";
@@ -21,9 +19,9 @@ function mood() {
 }
 
 function greaterThan10(num) {
-  let isBigEnough;
+  let isBigEnough = 10;
 
-  if (isBigEnough) {
+  if (num > isBigEnough) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
@@ -31,8 +29,8 @@ function greaterThan10(num) {
 }
 
 function get3rdIndex(arr) {
-  let index = 3;
-  let element;
+  let index = arr[3];
+
 
   return element;
 }
