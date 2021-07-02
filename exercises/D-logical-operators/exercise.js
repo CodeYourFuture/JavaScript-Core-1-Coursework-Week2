887080a34996b86a2b10def108f020941b23f54d
Seyed Sharifi <seyedsharifi12@gmail.com>
2021-07-02 14:27:30 +0100

    made a change

--- exercises/D-logical-operators/exercise.js
+++ exercises/D-logical-operators/exercise.js
@@ -18,8 +18,8 @@ const cssLevelAbove5 = cssLevel > 5;
 // Finish the next two statement
 // Use the previous variables and logical operators
 // Do not "hardcode" the answers
-const cssAndHtmlAbove5 = htmlLevel > 5 && cssLevel > 5;
-const cssOrHtmlAbove5 = htmlLevel > 5 || cssLevel > 5;
+const cssAndHtmlAbove5 = htmlLevelAbove5 && cssLevelAbove5;
+const cssOrHtmlAbove5 = htmlLevelAbove5 || cssLevelAbove5;
 
 /* 
   DO NOT EDIT BELOW THIS LINE
