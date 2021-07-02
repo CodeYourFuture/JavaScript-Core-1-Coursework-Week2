e2d26e33b440b66cbd8f377de84c61ac70164fef
Mor Khojasteh <mori.kh1987@gmail.com>
2021-07-02 19:43:24 +0100

    G-conditionals-2

--- exercises/G-conditionals-2/exercise-1.js
+++ exercises/G-conditionals-2/exercise-1.js
@@ -6,7 +6,9 @@
   - if number is more or equal to zero, return the word "positive"
 */
 
-function negativeOrPositive(number) {}
+function negativeOrPositive(number) {
+  return (number >= 0) ? 'positive' : 'negative';
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
