2b3e844fd67e9c7be86ac4db6164ed0d4049f729
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-07-02 12:45:01 +0100

    mandatory 3

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -13,11 +13,25 @@
   5. How many times is "f2" called?
   6. What value does the "a" parameter take in the first "f1" call?
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
-*/
+
+----------------------------------------------------------------------------------------------------
+Answers :
+
+  1. b has not been defined line 34
+  2. Commented out line 45
+  3. 2, 6, 4, 9, 6, 13, 8
+  4. Twice (1st output is 9, 2nd is 13) 
+  5. 3 times (1st output 4, 2nd is 6, 3rd is 8)
+  6. 1 as "a" takes value of i, which is a counter
+  7. a=8
+
+
+
+  */
 
 let x = 2;
 let a = 6;
-
+// b is not defined
 const f1 = function (a, b) {
   return a + b;
 };
@@ -28,7 +42,7 @@ const f2 = function (a, b) {
 
 console.log(x);
 console.log(a);
-console.log(b);
+//console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
