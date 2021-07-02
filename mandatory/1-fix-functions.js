ebf890dd8c2c483a56f2fd04a124736d1bdff333
Mohammedal-abdulhamid <mohammedalabdulhamid@gmail.com>
2021-07-02 23:17:45 +0100

    H,I,J,J,extra and mandatory

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -11,9 +11,9 @@
 */
 
 function mood() {
-  let isHappy = true;
+  let isHappy;
 
-  if (isHappy) {
+  if ((isHappy = true)) {
     return "I am happy";
   } else {
     return "I am not happy";
@@ -21,9 +21,7 @@ function mood() {
 }
 
 function greaterThan10(num) {
-  let isBigEnough;
-
-  if (isBigEnough) {
+  if (num > 10) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
@@ -31,10 +29,7 @@ function greaterThan10(num) {
 }
 
 function get3rdIndex(arr) {
-  let index = 3;
-  let element;
-
-  return element;
+  return arr[3];
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
