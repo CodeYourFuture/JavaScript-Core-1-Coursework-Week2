87179eaabeb16c8f3d591b0b0773c5d03ae11c89
Mor Khojasteh <mori.kh1987@gmail.com>
2021-07-04 23:46:30 +0100

    Update

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -28,13 +28,13 @@ const f2 = function (a, b) {
 
 console.log(x);
 console.log(a);
-console.log(b);
+//console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
   if (i % 2 === 0) {
     const d = f2(i, x);
-    console.log(d); // have to declare "b"
+    console.log(d); 
   } else {
     const e = f1(i, a);
     console.log(e);
