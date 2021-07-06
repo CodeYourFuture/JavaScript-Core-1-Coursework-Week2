826cfd5d64046e16f51462bebe9c05cc9b3da594
Ibrahim Mattar <ibmmtr@gmail.com>
2021-07-06 06:28:21 +0300

    MAN-F3

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -6,13 +6,13 @@
 
   Answer the following questions:
 
-  1. This program throws an error. Why? (If you can't find it, try executing it).
-  2. Remove the line that throws the error.
-  3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
-  7. What is the value of the "a" outer variable when "f1" is called for the first time?
+  1. This program throws an error. Why? (If you can't find it, try executing it). line 26
+  2. Remove the line that throws the error. remove x variable
+  3. What is printed to the console? 8
+  4. How many times is "f1" called?  1 
+  5. How many times is "f2" called? 1 
+  6. What value does the "a" parameter take in the first "f1" call? 1
+  7. What is the value of the "a" outer variable when "f1" is called for the first time? 8
 */
 
 let x = 2;
@@ -23,12 +23,12 @@ const f1 = function (a, b) {
 };
 
 const f2 = function (a, b) {
-  return a + b + x;
+  return a + b; //+ x; error here
 };
 
 console.log(x);
 console.log(a);
-console.log(b);
+//console.log(b); //error here no variable b => only parameters and local scope
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
