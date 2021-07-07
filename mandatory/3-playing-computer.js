b4925028ba2258101214b458ff5f0758fdaa1b20
ISLEEM75 <isleem52@gmail.com>
2021-07-07 17:06:24 +0300

    JavaScript Exercises for week 2

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -6,37 +6,37 @@
 
   Answer the following questions:
 
-  1. This program throws an error. Why? (If you can't find it, try executing it).
-  2. Remove the line that throws the error.
-  3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
-  7. What is the value of the "a" outer variable when "f1" is called for the first time?
+  1. This program throws an error. Why? (If you can't find it, try executing it).  line 31 b not declare 
+  2. Remove the line that throws the error. line 31 
+  3. What is printed to the console? 2 6 4 9 6 13 8
+  4. How many times is "f1" called? 2 times
+  5. How many times is "f2" called? 3 times
+  6. What value does the "a" parameter take in the first "f1" call? 8
+  7. What is the value of the "a" outer variable when "f1" is called for the first time? 9
 */
 
 let x = 2;
 let a = 6;
 
-const f1 = function (a, b) {
-  return a + b;
+const f1 = function(a, b) {
+    return a + b;
 };
 
-const f2 = function (a, b) {
-  return a + b + x;
+const f2 = function(a, b) {
+    return a + b + x;
 };
 
 console.log(x);
 console.log(a);
-console.log(b);
+// console.log(b); b
 
 for (let i = 0; i < 5; ++i) {
-  a = a + 1;
-  if (i % 2 === 0) {
-    const d = f2(i, x);
-    console.log(d);
-  } else {
-    const e = f1(i, a);
-    console.log(e);
-  }
+    a = a + 1;
+    if (i % 2 === 0) {
+        const d = f2(i, x);
+        console.log(d);
+    } else {
+        const e = f1(i, a);
+        console.log(e);
+    }
 }
