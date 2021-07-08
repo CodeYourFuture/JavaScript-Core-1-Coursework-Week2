218bf1cbf9253dcd7ca5d3a9ff1f7d3deca9a80b
Khadar Dagal <57036418+Kdagaal@users.noreply.github.com>
2021-07-08 23:40:42 +0100

    JS2 exercises I-array-properties

--- exercises/I-array-properties/exercise.js
+++ exercises/I-array-properties/exercise.js
@@ -6,7 +6,11 @@
 */
 
 function isEmpty(arr) {
-  return; // complete this statement
+    if (arr.length === 0) {
+        return true + ": This is an empty array!";
+    }
+    return false + ": This is not an empty array!";
+
 }
 
 /* 
