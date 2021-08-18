20a82d50303f63b65e62014d8ea35b6002611eaa
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-08-18 18:27:28 +0100

    Update exercise.js
    Daniel's role checked.

--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -5,10 +5,16 @@
   If Daniel is a mentor, print out "Hi, I'm Daniel, I'm a mentor."
   If Daniel is a student, print out "Hi, I'm Daniel, I'm a student."
 */
-
+function checkRole(){
 var name = "Daniel";
 var danielsRole = "mentor";
-
+if (danielsRole === "mentor"){
+  return `Hi, I'm ${name}, I am a ${danielsRole}.`
+} else if (danielsRole === "student"){
+  return `Hi, I'm ${name}, I am a student.`
+}
+}
+console.log(checkRole());
 /* 
 EXPECTED RESULT
 ---------------
