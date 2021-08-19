6be49b4d4d102d229ee4af01fddc393ea3107584
smkamaldar <smkamaldar@gmail.com>
2021-08-19 19:42:18 +0100

    mandatory folder has been solved

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,12 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === "number" && num % 2 === 0 && num <= 100) {
+    return true;
+  }
+  return false;
+}
 
 /*
 Write a function that:
@@ -16,7 +21,10 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+  let value = num > 100 ? 100 : Math.round(num * 100) / 100;
+  return `${value}%`;
+}
 
 /*
 Write a function that:
@@ -25,7 +33,14 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+function tidyUpStrings(arrayOfStrings) {
+  for (let i = 0; i < arrayOfStrings.length; i++) {
+    // we can call multiple method as a chain beacuse each of them return an output ,so another one takes
+    // that output as an input.
+    arrayOfStrings[i] = arrayOfStrings[i].toLowerCase().replace("/", "").trim();
+  }
+  return arrayOfStrings;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -77,7 +92,7 @@ test("tidyUpString function works", () => {
       "ashleigh   ",
       "   Alastair  ",
       " anne marie  ",
-    ])  
+    ])
   ).toEqual([
     "daniel",
     "sanyia",
@@ -87,5 +102,5 @@ test("tidyUpString function works", () => {
     "ashleigh",
     "alastair",
     "anne marie",
-  ]); 
+  ]);
 });
