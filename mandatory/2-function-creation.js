a7da933ef7202c4b05cf093e69e10f30eaf66bfb
MireilleNyoubissi <nmireillemat@yahoo.fr>
2021-08-20 00:02:33 +0100

    completed mandatory

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,9 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  return typeof num === "number" && num%2=== 0 && (num <= 100);
+}
 
 /*
 Write a function that:
@@ -16,7 +18,16 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage (num) {
+  if(Number.isInteger(num)) {
+    return num.toString() + "%";
+  }else if (num >= 100) {
+    return "100%";
+  } else {
+    return Math.round(num * 100) / 100 + "%";
+  }
+}
+
 
 /*
 Write a function that:
@@ -25,7 +36,14 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+function tidyUpStrings(arrayOfStrings) {
+  var arr = [];
+  for(let i = 0; i < arrayOfStrings.length; i++){
+    arr.push(arrayOfStrings[i].toLowerCase().trim().split("/").join(""));
+  }
+  return arr;
+
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
