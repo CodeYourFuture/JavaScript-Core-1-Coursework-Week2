71c231575677725aad448450d7a6cf3ce1a1f14e
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2021-08-19 17:46:28 +0100

    exercises_E_donditionals_finished

--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -6,8 +6,14 @@
   If Daniel is a student, print out "Hi, I'm Daniel, I'm a student."
 */
 
-var name = "Daniel";
+var firstName = "Daniel";
 var danielsRole = "mentor";
+
+console.log(
+  danielsRole == "mentor"
+    ? "Hi, I'm " + firstName + ", I'm a " + danielsRole + "."
+    : "Hi, I'm " + firstName + ", I'm a student."
+);
 
 /* 
 EXPECTED RESULT
