1e330a6bebd6f8862b6eb89349284a8d02cf4816
Kate <professionalkateg@gmail.com>
2021-08-20 11:40:50 +0100

    Completed all the exercises
    Everything in the exercises folder is complete

--- exercises/J-array-get-set/exercise.js
+++ exercises/J-array-get-set/exercise.js
@@ -5,11 +5,12 @@
 */
 
 function first(arr) {
-  return; // complete this statement
+  return arr[0]; // complete this statement
 }
 
 function last(arr) {
-  return; // complete this statement
+  // .length is 1 based while arrays are 0 based, so we need to take away one, otherwise it would be undefined
+  return arr[arr.length - 1]; // complete this statement
 }
 
 /* 
