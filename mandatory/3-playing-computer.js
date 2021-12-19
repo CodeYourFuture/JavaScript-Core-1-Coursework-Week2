ddc65377c7a7199a4ad8715f65df76f7995b4575
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2021-12-19 09:44:20 +0000

    Update 3-playing-computer.js

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -9,10 +9,12 @@
   1. This program throws an error. Why? (If you can't find it, try executing it).
   2. Remove the line that throws the error.
   3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
-  7. What is the value of the "a" outer variable when "f1" is called for the first time?
+  4. How many times is "f1" called? 
+          2 times when i=1,3
+  5. How many times is "f2" called? 
+          3times when i=0,2,4
+  6. What value does the "a" parameter take in the first "f1" call?  i=1
+  7. What is the value of the "a" outer variable when "f1" is called for the first time?  8
 */
 
 let x = 2;
@@ -25,10 +27,10 @@ const f1 = function (a, b) {
 const f2 = function (a, b) {
   return a + b + x;
 };
-
+// console.log print initial x = 6.
 console.log(x);
+// console.log print initial a = 6.
 console.log(a);
-console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
