177845191814ed6a92044873bc870a49f5da46a1
Selcuk Karakus <selcukkarakus@Selcuks-MacBook-Pro.local>
2021-12-21 16:35:34 +0100

    Create function that return first and last index.

--- exercises/J-array-get-set/exercise.js
+++ exercises/J-array-get-set/exercise.js
@@ -4,12 +4,12 @@
   Complete the functions below to get the first and last values from the array
 */
 
-function first(arr) {
-  return; // complete this statement
+const first = (arr) => {
+  return arr[0]; // complete this statement
 }
 
-function last(arr) {
-  return; // complete this statement
+const last = (arr) => {
+  return arr[arr.length -1]; // complete this statement
 }
 
 /* 
