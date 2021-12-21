2a137d8369577f9937cba85290e904af7737ea89
Selcuk Karakus <selcukkarakus@Selcuks-MacBook-Pro.local>
2021-12-21 15:49:07 +0100

    Create a function that works out grade.

--- exercises/G-conditionals-2/exercise-3.js
+++ exercises/G-conditionals-2/exercise-3.js
@@ -8,7 +8,17 @@
   - Otherwise the grade is "F"
 */
 
-function calculateGrade(mark) {}
+const calculateGrade = (mark) => {
+  if (mark >= 80) {
+    return `A`;
+  } else if (mark > 60 && mark < 80) {
+    return `B`;
+  } else if (mark <= 60 && mark >= 50) {
+    return `C`;
+  } else {
+    return `F`;
+  }
+};
 
 /* 
 DO NOT EDIT BELOW THIS LINE
