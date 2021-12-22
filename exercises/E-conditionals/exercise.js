b92e8cce0df38b8aea395669463548cc8441879a
mahri09 <63245237+mahri09@users.noreply.github.com>
2021-12-22 12:26:31 +0000

    exercises completed

--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -8,7 +8,11 @@
 
 let name = "Daniel";
 let danielsRole = "mentor";
-
+if (danielsRole === "mentor") {
+  console.log("Hi, I'm Daniel, I'm a mentor.");
+} else if (danielsRole === "student") {
+  console.log("Hi, I'm Daniel, I'm a student.");
+}
 /* 
 EXPECTED RESULT
 ---------------
