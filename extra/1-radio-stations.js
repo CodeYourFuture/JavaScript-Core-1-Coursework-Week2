5cf5f71bf722c8d1a1c63d98e9ef50a33c8cd260
Selcuk Karakus <selcukkarakus@Selcuks-MacBook-Pro.local>
2021-12-23 14:38:50 +0100

    Create a function that finds available radio stations.

--- extra/1-radio-stations.js
+++ extra/1-radio-stations.js
@@ -14,6 +14,13 @@
  */
 
 // `getAllFrequencies` goes here
+const getAllFrequencies = () => {
+  let frequencyList = [
+    87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104,
+    105, 106, 107, 108,
+  ];
+  return frequencyList;
+};
 
 /**
  * Next, let's write a function that gives us only the frequencies that are radio stations.
@@ -25,6 +32,15 @@
  * - Return only the frequencies that are radio stations.
  */
 // `getStations` goes here
+const getStations = () => {
+  let availableFrequencies = getAllFrequencies();
+
+  return availableFrequencies.filter((element) => {
+    return isRadioStation(element);
+  });
+};
+
+// console.log(getStations());
 
 /*
  * ======= TESTS - DO NOT MODIFY =======
