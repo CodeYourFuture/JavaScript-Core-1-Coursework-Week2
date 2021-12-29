488fa49288968bb363317551eba6bf84a6b148d8
Teniolao <tennybetty100@gmail.com>
2021-12-29 22:56:36 +0000

    extra

--- extra/1-radio-stations.js
+++ extra/1-radio-stations.js
@@ -14,6 +14,11 @@
  */
 
 // `getAllFrequencies` goes here
+function getAllFrequencies(){
+  let arrayOfFrequencies = [87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104,
+    105, 106, 107, 108];
+    return arrayOfFrequencies
+};
 
 /**
  * Next, let's write a function that gives us only the frequencies that are radio stations.
@@ -25,6 +30,9 @@
  * - Return only the frequencies that are radio stations.
  */
 // `getStations` goes here
+function getStations(){
+  return getAllFrequencies = isRadioStation;
+};
 
 /*
  * ======= TESTS - DO NOT MODIFY =======
