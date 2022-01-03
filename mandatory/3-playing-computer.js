68383a81777d7ae9ef1d382009c46f57c3c5e27b
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-01-03 23:25:08 +0000

    answered playing computer games questions

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -6,37 +6,36 @@
 
   Answer the following questions:
 
-  1. This program throws an error. Why? (If you can't find it, try executing it).
+  1. This program throws an error. Why? (If you can't find it, try executing it). B IS NOT DEFINED
   2. Remove the line that throws the error.
   3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
-  7. What is the value of the "a" outer variable when "f1" is called for the first time?
+  4. How many times is "f1" called? 5 times
+  5. How many times is "f2" called? twice
+  6. What value does the "a" parameter take in the first "f1" call? 6
+  7. What is the value of the "a" outer variable when "f1" is called for the first time? 9
 */
 
 let x = 2;
 let a = 6;
 
-const f1 = function (a, b) {
-  return a + b;
+const f1 = function (a, x) {
+  return a + x;
 };
 
-const f2 = function (a, b) {
-  return a + b + x;
+const f2 = function (a, x) {
+  return a + x + x;
 };
 
 console.log(x);
 console.log(a);
-console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
   if (i % 2 === 0) {
     const d = f2(i, x);
     console.log(d);
-  } else {
-    const e = f1(i, a);
-    console.log(e);
+   } else {
+     const e = f1(i, a);
+     console.log(e);
   }
 }
---
+++ mandatory/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+else {
+  //   const e = f1(i, a);
+  //   console.log(e);
+  // }
