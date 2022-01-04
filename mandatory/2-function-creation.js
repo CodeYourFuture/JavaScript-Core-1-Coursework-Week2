d44452acc7b68703234f56e6e634927a7c19b1b6
mairacagri <70014030+mairacagri@users.noreply.github.com>
2022-01-04 12:50:48 +0000

    mandatory folder

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,11 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === "number" && num % 2 === 0 && num <= 100){
+    return true;
+  }
+}
 
 /*
 Write a function that:
@@ -16,7 +20,15 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+   if (num > 100) {
+     return 100 
+   } else {
+     return `${num.toFixed(2)} %`
+   }
+
+
+}
 
 /*
 Write a function that:
@@ -24,8 +36,19 @@ Write a function that:
 - removes any spaces in the beginning or end of each string
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
+
+https://blog.logrocket.com/javascript-array-methods/
 */
-function tidyUpStrings(arrayOfStrings) {}
+
+function tidyUpStrings(arrayOfStrings) {
+  let newArr = arrayOfStrings.map(items) 
+     return (newArr.trim() ) 
+            (newArr.toLowerCase() )
+            (newArr.replace("/", "") )
+           
+  }
+  
+
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -6,23 +6,23 @@
 
   Answer the following questions:
 
-  1. This program throws an error. Why? (If you can't find it, try executing it).
-  2. Remove the line that throws the error.
+  1. This program throws an error. Why? (If you can't find it, try executing it). missing b variable
+  2. Remove the line that throws the error. line 31
   3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
+  4. How many times is "f1" called? called 2 time (line 30,31)
+  5. How many times is "f2" called? called 3 times (line 29-31)
+  6. What value does the "a" parameter take in the first "f1" call? value 6 ? not sure
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
 */
 
 let x = 2;
 let a = 6;
 
-const f1 = function (a, b) {
+const f1 = function(a, b) {
   return a + b;
 };
 
-const f2 = function (a, b) {
+const f2 = function(a, b) {
   return a + b + x;
 };
 
@@ -30,7 +30,7 @@ console.log(x);
 console.log(a);
 console.log(b);
 
-for (let i = 0; i < 5; ++i) {
+for (let i = 0; i < 5; i++) {
   a = a + 1;
   if (i % 2 === 0) {
     const d = f2(i, x);
