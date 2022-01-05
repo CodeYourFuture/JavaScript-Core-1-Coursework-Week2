f5014be7cc44aa7e976781b6f84090356d3f5cee
AliNurisiahsiahi <alinurisiahsiahi@gmail.com>
2022-01-05 23:49:26 +0000

    Some change and finish mandatory

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -7,12 +7,18 @@
   Answer the following questions:
 
   1. This program throws an error. Why? (If you can't find it, try executing it).
+  consol.log(b); throws error because we don't have b variable
   2. Remove the line that throws the error.
   3. What is printed to the console?
+    2, 6, 4, 9, 6, 13, 8.
   4. How many times is "f1" called?
+    Two Times printed  to the console 9 and 13.
   5. How many times is "f2" called?
+    Three times printed to the console 4, 6, 8.
   6. What value does the "a" parameter take in the first "f1" call?
+    i = 1 and  go to the parameter a = 1.
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
+      a = 7 + 1 = 8.
 */
 
 let x = 2;
@@ -28,8 +34,6 @@ const f2 = function (a, b) {
 
 console.log(x);
 console.log(a);
-console.log(b);
-
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
   if (i % 2 === 0) {
