faf8ee733df1f8bcfc26569ad1ff13b53f6a4f4b
Hatice <91030028+Hatice06@users.noreply.github.com>
2022-01-05 22:22:07 +0000

    Mandatory homework completed

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -7,12 +7,19 @@
   Answer the following questions:
 
   1. This program throws an error. Why? (If you can't find it, try executing it).
+  Answer: Line 32 throws an error because "b" is not defined
   2. Remove the line that throws the error.
+  Done
   3. What is printed to the console?
+  Answer: The values a and x and the results of f1 and f2 functions.
   4. How many times is "f1" called?
+  Answer: 2 times
   5. How many times is "f2" called?
+  Answer: 3 times
   6. What value does the "a" parameter take in the first "f1" call?
+  Answer: 7
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
+  Answer: 8
 */
 
 let x = 2;
@@ -28,7 +35,7 @@ const f2 = function (a, b) {
 
 console.log(x);
 console.log(a);
-console.log(b);
+//console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
@@ -40,3 +47,5 @@ for (let i = 0; i < 5; ++i) {
     console.log(e);
   }
 }
+
+
