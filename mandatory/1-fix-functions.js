884c3c22fbf588f52f1a1e81da7ae813760f49f9
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2022-01-13 09:26:03 +0000

    Refactor condition.
    Store the condition in a variable based on the value num.

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -20,9 +20,9 @@ function mood(isHappy) {
 }
 
 function greaterThan10(num) {
-  let isBigEnough = num;
+  let isBigEnough = num > 10;
 
-  if (isBigEnough > 10) {
+  if (isBigEnough) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
