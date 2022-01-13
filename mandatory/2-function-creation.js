57b38e40c373ce42e6688a440958500ca08916a2
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2022-01-13 09:36:35 +0000

    Refactor if else logic.
    Return expression straight away. Remove if else.

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -7,11 +7,7 @@ Tip: use logical operators
 */
 
 const validate = (num) => {
-  if (typeof num === "number" && num % 2 === 0 && num <= 100) {
-    return true;
-  } else {
-    return false;
-  }
+  return (typeof num === "number" && num % 2 === 0 && num <= 100);
 };
 
 /*
