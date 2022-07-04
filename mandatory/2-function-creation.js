dce37f0b6d00d8057e5fa3035b54dbcef84fa9fc
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2022-07-04 23:47:12 +0200

    completed mandatory 2 - 3

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,13 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === "number" && num % 2 === 0 && num <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /*
 Write a function that:
@@ -16,7 +22,13 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+  result = num;
+  if (result > 100) {
+    let resultOver100 = 100;
+    return Math.round(resultOver100 * 100) / 100 + "%";
+  } else return Math.round(result * 100) / 100 + "%";
+}
 
 /*
 Write a function that:
@@ -25,7 +37,11 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+
+function tidyUpStrings(arrayOfStrings) {
+  string = arrayOfStrings;
+  return strings.toUpperCase();
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -77,7 +93,7 @@ test("tidyUpString function works", () => {
       "ashleigh   ",
       "   Alastair  ",
       " anne marie  ",
-    ])  
+    ])
   ).toEqual([
     "daniel",
     "sanyia",
@@ -87,5 +103,5 @@ test("tidyUpString function works", () => {
     "ashleigh",
     "alastair",
     "anne marie",
-  ]); 
+  ]);
 });
