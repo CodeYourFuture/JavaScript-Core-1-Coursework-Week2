607318aa44b4bfd88ffbfbfc1f6a2b66bca9ae76
mathias02 <matthiasm2016@gmail.com>
2022-07-05 09:27:11 +0200

    mistakes fixe in file one

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,12 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+
+  if((num == num) && (num % 2 == 0) && (num <= 100)){
+      return "I won";
+  }
+}
 
 /*
 Write a function that:
@@ -16,7 +21,10 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+
+  return toString(num) + "%";
+}
 
 /*
 Write a function that:
@@ -25,7 +33,10 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+function tidyUpStrings(arrayOfStrings) {
+
+  arrayOfStrings.toLowerCase().trim();
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
