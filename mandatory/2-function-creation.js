ad369bc17a5d35fef3edcda2b9b06b7747faefcd
Kabelo Letlala <86678850+M3ntalist@users.noreply.github.com>
2022-07-05 13:48:54 +0200

    Updated mandatory coursework on js

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,13 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === "number" && num % 2 === 0 &&  num <= 100){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /*
 Write a function that:
@@ -16,7 +22,15 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 100) {
+      arr[i] = 100;
+    }
+     arr[i] = Math.round(arr[i] * 100) / 100 +"%";
+      }
+     return arr;
+}
 
 /*
 Write a function that:
@@ -25,7 +39,19 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+function tidyUpStrings(arrayOfStrings) {
+ let removeSpace;
+  let removeForwardSlash;
+  let returnLowerCase;
+
+ for (let i = 0; i < strArr.length; i++) {
+   removeSpace = strArr[i].trim();
+   removeForwardSlash = removeSpace.replace("/", "");
+   returnLowerCase = removeForwardSlash.toLowerCase();
+   strArr[i] = returnLowerCase;
+ }
+  return strArr;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -16,27 +16,20 @@
 */
 
 let x = 2;
-let a = 6;
-
-const f1 = function (a, b) {
-  return a + b;
-};
-
-const f2 = function (a, b) {
-  return a + b + x;
-};
-
 console.log(x);
 console.log(a);
-console.log(b);
+//console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
+  //console.log("value", a);
   if (i % 2 === 0) {
     const d = f2(i, x);
+    //console.log("search", x);
     console.log(d);
   } else {
     const e = f1(i, a);
+     //console.log("find", a);
     console.log(e);
   }
 }
