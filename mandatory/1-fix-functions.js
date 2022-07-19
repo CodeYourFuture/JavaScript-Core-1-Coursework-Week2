1cab1b4df9972b8190aa3508ce142a42496010c2
Patou Malongi Mabando <patoumabando@gmail.com>
2022-07-19 13:48:09 +0200

    ZA2-Patou Mabando
    Js Core-1 Coursework Week2 resolution.

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,9 +10,7 @@
 
 */
 
-function mood() {
-  let isHappy = true;
-
+function mood(isHappy) {
   if (isHappy) {
     return "I am happy";
   } else {
@@ -21,9 +19,9 @@ function mood() {
 }
 
 function greaterThan10(num) {
-  let isBigEnough;
+  let x = 10;
 
-  if (isBigEnough) {
+  if (x < num) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
@@ -31,8 +29,7 @@ function greaterThan10(num) {
 }
 
 function get3rdIndex(arr) {
-  let index = 3;
-  let element;
+  let element = arr[3];
 
   return element;
 }
--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,13 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === "number" && num % 2 == 0 && num <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /*
 Write a function that:
@@ -16,7 +22,13 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+  if (num > 100) {
+    return 100 + "%";
+  } else {
+    return `${num.toFixed(2) / 1}%`;
+  }
+}
 
 /*
 Write a function that:
@@ -25,7 +37,18 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+function tidyUpStrings(arrayOfStrings) {
+  let removeSpace;
+  let removeForwardSlash;
+  let returnLowerCase;
+  for (let i = 0; i < arrayOfStrings; i++) {
+    arrayOfStrings[i].trim();
+    removeForwardSlash = removeSpace.replace("/", "");
+    returnLowerCase = removeForwardSlash.toLowerCase();
+    arrayOfStrings[i] = returnLowerCase;
+  }
+  return arrayOfStrings;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -62,7 +85,7 @@ test.each([
   [18.103, "18.1%"],
   [187.2, "100%"],
   [0.372, "0.37%"],
-])("formatPercentage function works for %s", (input, expected) => {
+])("formatPercentage function works for %", (input, expected) => {
   expect(formatPercentage(input)).toEqual(expected);
 });
 
@@ -77,7 +100,7 @@ test("tidyUpString function works", () => {
       "ashleigh   ",
       "   Alastair  ",
       " anne marie  ",
-    ])  
+    ])
   ).toEqual([
     "daniel",
     "sanyia",
@@ -87,5 +110,5 @@ test("tidyUpString function works", () => {
     "ashleigh",
     "alastair",
     "anne marie",
-  ]); 
+  ]);
 });
