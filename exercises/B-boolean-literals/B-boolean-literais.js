61a9ad2f1a4ab8d8d41ca13cf5f02d7de3390bbb
kavitappatil <patil.poem@gmail.com>
2022-08-07 22:30:08 +0530

    B-boolean-literaisjs

---
+++ exercises/B-boolean-literals/B-boolean-literais.js
@@ -0,0 +1,9 @@
+let codeYourFutureIsGreat = true;
+let mozafarIsCool = false;
+let calculationCorrect = true;
+let moreThan10Students = false;
+
+console.log("Is Code Your Future great?", codeYourFutureIsGreat);
+console.log("Is Mozafar cool?", mozafarIsCool);
+console.log("Does 1 + 1 = 2?", calculationCorrect);
+console.log("Are there more than 10 students?", moreThan10Students);
