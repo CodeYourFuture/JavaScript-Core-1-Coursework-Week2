9a74de5cd72815e2187ef7a587cbbc8e36ab5fbb
ozibozkurt <ozipaki@gmail.com>
2022-08-08 18:53:27 +0100

    mandatory-3

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -17,18 +17,15 @@
 
 let x = 2;
 let a = 6;
-
 const f1 = function (a, b) {
   return a + b;
 };
-
 const f2 = function (a, b) {
   return a + b + x;
 };
 
 console.log(x);
 console.log(a);
-console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
