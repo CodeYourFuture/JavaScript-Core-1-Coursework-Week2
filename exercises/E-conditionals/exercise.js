84a7c3bc38304ff98ef3534db16adf7c0321df45
ozibozkurt <ozipaki@gmail.com>
2022-08-08 13:06:08 +0100

    E-conditionals

--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -8,6 +8,13 @@
 
 let name = "Daniel";
 let danielsRole = "mentor";
+if (danielsRole === "mentor"){
+  console.log("Hi, I'm " + name + ", I'm a mentor.");
+} else {
+  console.log("Hi, I'm " + name + ", I'm a student.");
+}
+
+
 
 /* 
 EXPECTED RESULT
