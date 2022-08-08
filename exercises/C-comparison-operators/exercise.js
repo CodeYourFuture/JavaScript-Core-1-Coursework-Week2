0695c1890d865331d459e3e7a3cd435fce61b70a
ozibozkurt <ozipaki@gmail.com>
2022-08-08 11:23:59 +0100

    B-boolean-literals

--- exercises/C-comparison-operators/exercise.js
+++ exercises/C-comparison-operators/exercise.js
@@ -5,16 +5,8 @@
   The variables should have values that match the expected results.
 */
 
-let studentCount = 16;
-let mentorCount = 9;
-let moreStudentsThanMentors; // finish this statement
 
-let roomMaxCapacity = 25;
-let enoughSpaceInRoom; // finish this statement
 
-let personA = "Daniel";
-let personB = "Irina";
-let sameName; // finish this statement
 
 /* 
   DO NOT EDIT BELOW THIS LINE
