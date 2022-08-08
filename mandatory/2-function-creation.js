1f994c359915b173466b00f44757e83a3d62f52c
vanipriya_piramanathan <vpriyaarun23@gmail.com>
2022-08-08 20:48:39 +0100

    mandatory-folder-completed

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,10 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+
+function validate(num) {
+  return typeof num==="number" && num%2===0 && num<=100 ? true : false;
+}
 
 /*
 Write a function that:
@@ -16,7 +19,15 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+  if(num>100){
+    return "100%"
+  }else if(num%1!==0){
+    return Math.round((num + Number.EPSILON) * 100) / 100 +"%"
+  } else{
+    return `${num}%`
+  }
+}
 
 /*
 Write a function that:
@@ -25,7 +36,10 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+function tidyUpStrings(arrayOfStrings) {
+let correctedArray = arrayOfStrings.map(el => el.replaceAll("/", "").trim().toLowerCase());
+return correctedArray;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
