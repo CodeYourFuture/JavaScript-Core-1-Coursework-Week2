c538c822854062bc67655fe2f324470289e7ed2e
hanakisu <105117072+hanakisu@users.noreply.github.com>
2022-08-09 23:04:55 +0100

    exercise javascript week2

--- exercises/G-conditionals-2/exercise-3.js
+++ exercises/G-conditionals-2/exercise-3.js
@@ -8,7 +8,19 @@
   - Otherwise the grade is "F"
 */
 
-function calculateGrade(mark) {}
+function calculateGrade(mark) {
+  if (mark > 80) {
+    return "A"
+  } else if (mark > 60) {
+    return "B"
+  } else if(mark > 50){
+    return "c"
+  } else {
+    return "F"
+  }
+    
+  
+}
 
 /* 
 DO NOT EDIT BELOW THIS LINE
