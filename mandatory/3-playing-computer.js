a6f17c7f9483b191be01e789db58cd0c1e750919
simeonbikov <bikovsem@gmail.com>
2022-08-10 15:54:59 -0400

    completed mandatory ex.3

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -8,11 +8,11 @@
 
   1. This program throws an error. Why? (If you can't find it, try executing it).
   2. Remove the line that throws the error.
-  3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
-  7. What is the value of the "a" outer variable when "f1" is called for the first time?
+  3. What is printed to the console?   2, 6, 4, 9, 6, 13, 8
+  4. How many times is "f1" called?    2 
+  5. How many times is "f2" called?    3
+  6. What value does the "a" parameter take in the first "f1" call?   8
+  7. What is the value of the "a" outer variable when "f1" is called for the first time?  8
 */
 
 let x = 2;
@@ -26,17 +26,18 @@ const f2 = function (a, b) {
   return a + b + x;
 };
 
-console.log(x);
-console.log(a);
-console.log(b);
+console.log(x); //  2
+console.log(a); //  6
+// console.log(b);  b is not defined
 
 for (let i = 0; i < 5; ++i) {
-  a = a + 1;
+  // 0, 1, 2, 3, 4
+  a = a + 1; // 7, 8, 9, 10, 11
   if (i % 2 === 0) {
     const d = f2(i, x);
-    console.log(d);
+    console.log(d); // 0 + 2 + 2   |  2 + 2 + 2   |  4 + 2 + 2
   } else {
     const e = f1(i, a);
-    console.log(e);
+    console.log(e); // 1 + 8 |  3 + 10
   }
 }
