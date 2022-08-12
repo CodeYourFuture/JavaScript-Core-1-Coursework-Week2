8106d207e1939659d99b11862f2c7028f9c22215
micheale2021 <andemariammicheale@gmail.com>
2022-08-12 03:16:05 +0100

    exercise/J 1 & 2 array-get-set complete

--- exercises/J-array-get-set/exercise.js
+++ exercises/J-array-get-set/exercise.js
@@ -5,11 +5,13 @@
 */
 
 function first(arr) {
-  return; // complete this statement
-}
+  return arr[0] ; // complete this statement
+  
+};
 
 function last(arr) {
-  return; // complete this statement
+  const lastItem = arr[arr.length - 1]
+  return lastItem; // complete this statement
 }
 
 /* 
