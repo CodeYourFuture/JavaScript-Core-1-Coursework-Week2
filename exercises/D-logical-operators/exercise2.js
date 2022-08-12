838ec8066de2e69f52c00fde10c4ab17770c5398
Abdirahim Hussein <drahimhussein@gmail.com>
2022-08-12 16:44:23 +0100

    had to make a new fork to be able to make a good pull request copy and pasted my previous work

--- exercises/D-logical-operators/exercise2.js
+++ exercises/D-logical-operators/exercise2.js
@@ -4,8 +4,30 @@
   This program calls some functions that are either missing or incomplete.
   Update the code so that you get the expected result.
 */
+function isNegative(num) {
+  if (num < 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+function isBetween5and10(num){
+  if(num > 4 && num < 11 ){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
-function isNegative() {}
+function isShortName(name) {
+  return name.length <= 6;
+}
+
+function startsWithD(name) {
+  return name[0] === 'D'
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
