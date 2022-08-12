e60f1640dcedeafc91996bea1f7c91c72eb520c6
micheale2021 <andemariammicheale@gmail.com>
2022-08-12 14:22:27 +0100

    complete 1 fix functions complete
    complete 2 function creation
    
    complete 3 playing computer

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,32 +10,29 @@
 
 */
 
-function mood() {
-  let isHappy = true;
-
+function mood(isHappy) {
   if (isHappy) {
     return "I am happy";
   } else {
     return "I am not happy";
   }
-}
+}; 
 
 function greaterThan10(num) {
-  let isBigEnough;
+  // let isBigEnough = num;
 
-  if (isBigEnough) {
+  if (num > 10) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
   }
-}
+};
 
 function get3rdIndex(arr) {
-  let index = 3;
-  let element;
+  let element = arr[3];
 
   return element;
-}
+};
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
