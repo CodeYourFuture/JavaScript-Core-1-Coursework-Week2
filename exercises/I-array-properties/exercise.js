10db6bb2f80e480b343239a2e622b5a6ed7669dc
micheale2021 <andemariammicheale@gmail.com>
2022-08-12 03:15:11 +0100

    exercise/I array-properties complete

--- exercises/I-array-properties/exercise.js
+++ exercises/I-array-properties/exercise.js
@@ -6,8 +6,12 @@
 */
 
 function isEmpty(arr) {
-  return; // complete this statement
-}
+  if (arr.length > 0) {
+    return false; // complete this statement
+  } else {
+    return true;
+  } 
+};
 
 /* 
   DO NOT EDIT BELOW THIS LINE
