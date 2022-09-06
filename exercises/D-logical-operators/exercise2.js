490af249ca0bd29265a0bbf163acfc3579d0a900
Sharifa Ismail <105314490+Sharifa1@users.noreply.github.com>
2022-09-06 12:55:19 +0100

    Wk2 exercise solved

--- exercises/D-logical-operators/exercise2.js
+++ exercises/D-logical-operators/exercise2.js
@@ -5,7 +5,21 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
+function isNegative(num) {
+  return (num < 0) ? true : false;
+}
+
+function isBetween5and10(num){
+  return (num >= 5 && num <= 10) ? true : false;
+}
+
+function isShortName(name){
+  return (name.length <= 6) ? true : false;
+}
+
+function startsWithD (name) {
+  return (name[0] === "D") ? true : false;
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
