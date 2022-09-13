c0325a2bd5ff8263b3cdb348516c12f87ffee0cc
leilafaez <79403281+leilafaez@users.noreply.github.com>
2022-09-13 10:43:57 +0100

    update mandatory file

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,11 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === "number" && num % 2 === 0 && num <= 100) {
+    return true;
+  } else return false;
+}
 
 /*
 Write a function that:
@@ -16,7 +20,13 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+let result = Math.round(num * 100) / 100;
+if (result > 100) {
+  result = 100;
+}
+return result + "%";
+}
 
 /*
 Write a function that:
@@ -25,7 +35,13 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+function tidyUpStrings(arrayOfStrings) {
+    let result = arrayOfStrings.map(function (item) {
+      return item.trim().toLowerCase().replaceAll("/", "");
+    });
+    console.log(result);
+  }
+
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
