a92e311ef5641d47ddfbfc7063561c8bbefa8a85
Milad E-Pour <epour.milad@gmail.com>
2022-09-13 19:27:27 +0100

    Mandatory Done

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,13 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (num === Number && num % 2 === 0 && num <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /*
 Write a function that:
@@ -16,8 +22,10 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
-
+function formatPercentage(num) {
+  const numToreturn = num > 100 ? 100 : parseFloat(num.toFixed(2))
+  return numToreturn + "%"
+}
 /*
 Write a function that:
 - takes an array of strings as input
@@ -25,7 +33,10 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+function tidyUpStrings(arrayOfStrings) {
+  return arrayOfStrings.map((elements) =>
+     elements.trim().toLowerCase().replaceAll(/\//g, ""));
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -8,15 +8,16 @@
 
   1. This program throws an error. Why? (If you can't find it, try executing it).
   2. Remove the line that throws the error.
-  3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
+  3. What is printed to the console? 2,6
+  4. How many times is "f1" called? 3
+  5. How many times is "f2" called? 2
+  6. What value does the "a" parameter take in the first "f1" call? 4
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
 */
 
 let x = 2;
 let a = 6;
+let b = 4;
 
 const f1 = function (a, b) {
   return a + b;
@@ -34,9 +35,9 @@ for (let i = 0; i < 5; ++i) {
   a = a + 1;
   if (i % 2 === 0) {
     const d = f2(i, x);
-    console.log(d);
+    console.log(d, "f1");
   } else {
     const e = f1(i, a);
-    console.log(e);
+    console.log(e,"f2");
   }
 }
