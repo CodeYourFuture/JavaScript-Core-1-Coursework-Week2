2c80b263248af20cf64ae070dbec5236faf99e51
Doris-Siu <doris.siu.my@gmail.com>
2022-09-14 14:06:41 +0100

    Complete mandatory/2-function-creation.js

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,13 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === "number" && num % 2 === 0 && num <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /*
 Write a function that:
@@ -16,7 +22,13 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+  if (num > 100) {
+    num = 100;
+  } 
+  num = parseFloat(num.toFixed(2));
+  return `${num}%`;
+}
 
 /*
 Write a function that:
@@ -25,8 +37,14 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
-
+function tidyUpStrings(arrayOfStrings) {
+  let result = [];
+  for (const item of arrayOfStrings) {
+    let itemTmp = item.trim().replace("/", "").toLowerCase();
+    result.push(itemTmp);
+  }
+  return result;
+}
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 test("validate function accepts valid even number", () => {
@@ -77,7 +95,7 @@ test("tidyUpString function works", () => {
       "ashleigh   ",
       "   Alastair  ",
       " anne marie  ",
-    ])  
+    ])
   ).toEqual([
     "daniel",
     "sanyia",
@@ -87,5 +105,5 @@ test("tidyUpString function works", () => {
     "ashleigh",
     "alastair",
     "anne marie",
-  ]); 
+  ]);
 });
