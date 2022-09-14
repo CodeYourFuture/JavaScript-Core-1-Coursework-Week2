cf13a6096378b959b3525e852b1b0189afb70bca
Doris-Siu <doris.siu.my@gmail.com>
2022-09-14 11:20:10 +0100

    Complete mandatory/1-fix-functions.js

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,8 +10,7 @@
 
 */
 
-function mood() {
-  let isHappy = true;
+function mood(isHappy) {
 
   if (isHappy) {
     return "I am happy";
@@ -21,9 +20,9 @@ function mood() {
 }
 
 function greaterThan10(num) {
-  let isBigEnough;
+  let isBigEnough = num;
 
-  if (isBigEnough) {
+  if (isBigEnough >10) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
@@ -32,9 +31,9 @@ function greaterThan10(num) {
 
 function get3rdIndex(arr) {
   let index = 3;
-  let element;
+  let element= arr;
 
-  return element;
+  return element[index];
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
