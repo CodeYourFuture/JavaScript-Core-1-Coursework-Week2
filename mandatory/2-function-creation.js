311c1244b5132aee007464c46388af1aee9b630e
fatih-celebi <celebifatih.c1@gmail.com>
2022-09-15 02:28:43 +0100

    Complete Exercise-2

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,12 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === "number" && num % 2 === 0 && num <= 100) {
+    return true;
+} else {
+  return false;
+}
 
 /*
 Write a function that:
@@ -16,7 +21,16 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+  let decimalNumber = num.toFixed(2);
+  let str = `${decimalNumber}%`;
+
+  if (num > 100){
+    return 100;
+  } else {
+    return str;
+  }
+}
 
 /*
 Write a function that:
@@ -25,7 +39,12 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+
+function tidyUpStrings(arrayOfStrings) {
+  let trim = arrayOfStrings.trim();
+  let result = trim.replaceAll(`/`, ``).toLowercase();
+  return result;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
