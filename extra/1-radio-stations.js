2035b65a09ce27027cd9bff9684ce334408fca2e
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2022-09-16 19:01:10 +0100

    All done

--- extra/1-radio-stations.js
+++ extra/1-radio-stations.js
@@ -14,6 +14,13 @@
  */
 
 // `getAllFrequencies` goes here
+function getAllFrequencies() {
+  let listFrequencies = [];
+  for (let i = 87; i <= 108; i++) {
+    listFrequencies.push(i);
+  }
+  return listFrequencies;
+}
 
 /**
  * Next, let's write a function that gives us only the frequencies that are radio stations.
@@ -25,6 +32,9 @@
  * - Return only the frequencies that are radio stations.
  */
 // `getStations` goes here
+function getStations(listFrequencies) {
+  return getAllFrequencies().filter(isRadioStation);
+}
 
 /*
  * ======= TESTS - DO NOT MODIFY =======
