8f477460b976ae702f1886ac91ca8907750027f7
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-09-19 18:54:45 +0100

    Mandatory exercises

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -13,6 +13,42 @@
   5. How many times is "f2" called?
   6. What value does the "a" parameter take in the first "f1" call?
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
+
+
+Answers
+
+1. b is not declared.
+2. comment out console.log(b)
+3. console.log(x) // output 2
+   console.log(a) // output 6
+4. f1 is called two times
+ 
+    first time 
+    i = 1 because 1 % 2 = 1
+    and x = 2;
+
+    1 + 2 + 2 = 5,
+    ---------------
+    second time 
+    i = 3 because 3 % 2 = 1
+    3 + 2 + 2 = 7
+5. f2 is called three times
+   first time 
+    i = 0 because 0 % 2 = 0
+    and x = 2;
+
+    0 + 2 + 2 = 4,
+    ---------------
+
+    second time 
+    i = 2 because 2 % 2 = 0
+    and x = 2;
+
+    2 + 2 + 2 = 6,
+    ---------------
+    third time 
+    i = 4 because 4 % 2 = 0
+    4 + 2 + 2 = 8
 */
 
 let x = 2;
@@ -26,17 +62,17 @@ const f2 = function (a, b) {
   return a + b + x;
 };
 
-console.log(x);
-console.log(a);
-console.log(b);
+console.log(x,' just x');
+console.log(a, 'just a');
+// console.log(b);
 
-for (let i = 0; i < 5; ++i) {
+for (let i = 0; i < 5; i++) {
   a = a + 1;
   if (i % 2 === 0) {
     const d = f2(i, x);
-    console.log(d);
+    console.log(d, "f2");
   } else {
     const e = f1(i, a);
-    console.log(e);
+    console.log(e, 'f1');
   }
 }
