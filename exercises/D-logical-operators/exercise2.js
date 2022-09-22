34b98e40e8a3152453e230e723c186c181e82ec8
Adiba <adiba.du.bd@gmail.com>
2022-09-22 17:36:05 +0100

    complete exercises
    complete exercises

--- exercises/D-logical-operators/exercise2.js
+++ exercises/D-logical-operators/exercise2.js
@@ -5,8 +5,41 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
+function isNegative(number) {
+if ( number < 0){
+  return true;
+} else{
+  return false
+}
+}
 
+function isBetween5and10 (number){
+  if ( 5 <= number <= 10 ){
+    return true
+  } else{
+    return false
+  }
+
+}
+
+function isShortName (name){
+   let shortName =  name.split("").length
+   if ( shortName < 1){
+    return false
+   } else {
+    return true
+   }
+
+}
+
+function startsWithD (daniel){
+  if ( daniel.startsWith("D")){
+    return true
+  } else{
+    return false
+  }
+
+}
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -9,6 +9,14 @@
 let name = "Daniel";
 let danielsRole = "mentor";
 
+  if (danielsRole === "mentor"){
+    console.log( `Hi, I'm Daniel, I'm a ${danielsRole}`);
+  } else if (danielsRole === "student") {
+    console.log(`Hi, I'm Daniel, I'm a student`);
+  }
+
+
+
 /* 
 EXPECTED RESULT
 ---------------
