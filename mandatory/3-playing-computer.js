c5ef199e13d2fd086373eb5fc9df74f6fbb91eb6
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2022-11-20 21:51:43 +0000

    predictions for mandatory 3 complet

--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -21,22 +21,33 @@ let a = 6;
 const f1 = function (a, b) {
   return a + b;
 };
+// f1 is fine, all place holders used
 
 const f2 = function (a, b) {
   return a + b + x;
 };
 
+//expext f2 to return the sum of the place holders and x which equlas 2
 console.log(x);
 console.log(a);
 console.log(b);
+// expect cosole.log b to retrun undefined
 
 for (let i = 0; i < 5; ++i) {
+  //++i means i is increased first befor it is evalusted? i starts at 1
+  // insteas of zero?
   a = a + 1;
+  // a now = 7
   if (i % 2 === 0) {
+    //if i is even
     const d = f2(i, x);
+    // d = 7 + (2||4) + 2;
     console.log(d);
+    // d = 11 || 13
   } else {
     const e = f1(i, a);
+    // e = (1||3) + 7;
     console.log(e);
+    // e = 8 || 10;
   }
 }
