3afea31003cdfe0aa20bcc1b4054e939859e1ed4
Shayan Mahnam <95313895+Shayanma94@users.noreply.github.com>
2022-11-27 12:49:47 +0000

    exercises done

--- exercises/C-comparison-operators/exercise.js
+++ exercises/C-comparison-operators/exercise.js
@@ -7,14 +7,14 @@
 
 let studentCount = 16;
 let mentorCount = 9;
-let moreStudentsThanMentors; // finish this statement
+let moreStudentsThanMentors = studentCount > mentorCount; // finish this statement
 
 let roomMaxCapacity = 25;
-let enoughSpaceInRoom; // finish this statement
+let enoughSpaceInRoom = roomMaxCapacity >= mentorCount + studentCount; // finish this statement
 
 let personA = "Daniel";
 let personB = "Irina";
-let sameName; // finish this statement
+let sameName = personA === personB; // finish this statement
 
 /* 
   DO NOT EDIT BELOW THIS LINE
