c5e228ff32b801ac73e95f13e214024caf37e998
laxmikckarki <108900563+laxmikckarki@users.noreply.github.com>
2022-11-28 21:41:12 +0000

    done

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,10 +10,10 @@
 
 */
 
-function mood() {
+function mood(value) {
   let isHappy = true;
 
-  if (isHappy) {
+  if (isHappy === value) {
     return "I am happy";
   } else {
     return "I am not happy";
@@ -21,9 +21,9 @@ function mood() {
 }
 
 function greaterThan10(num) {
-  let isBigEnough = true;
+  let isBigEnough = num;
 
-  if (isBigEnough) {
+  if (isBigEnough > 10) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
@@ -32,9 +32,9 @@ function greaterThan10(num) {
 
 function get3rdIndex(arr) {
   let index = 3;
-  let element;
+  let element = index;
 
-  return element[index - 1];
+  return arr[element - 1];
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
