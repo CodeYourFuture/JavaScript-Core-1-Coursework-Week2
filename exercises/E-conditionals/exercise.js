3fe984d931e9485b69d076f0d40986c34a734651
Hengameh M.Khani <108895187+HeniMKH@users.noreply.github.com>
2022-11-28 22:56:27 +0000

    exersises and mandatory

--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -9,7 +9,12 @@
 let name = "Daniel";
 let danielsRole = "mentor";
 
-/* 
+if (name === danielsRole) {
+  console.log("Hi, I'm Daniel, I'm a mentor.");
+} else {
+  console.log("Hi, I'm Daniel, I'm a student.");
+}
+/*
 EXPECTED RESULT
 ---------------
 Hi, I'm Daniel, I'm a mentor.
