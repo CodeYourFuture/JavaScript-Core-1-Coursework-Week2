f142c0c03f1833afc9a7fa585a69e2d55fde67b9
elahe <106202329+elahemortazavi@users.noreply.github.com>
2022-11-28 10:05:29 +0000

    exer

--- exercises/D-logical-operators/exercise2.js
+++ exercises/D-logical-operators/exercise2.js
@@ -5,7 +5,34 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
+function isNegative(parameter) {
+  if (parameter < 0){
+    return true;
+  }
+  return false;
+}
+
+function isBetween5and10(num){
+if (num >= 5 && num <= 10) {
+  return true;
+}
+return false;
+}
+
+
+function isShortName(name) {
+  if (name.length<10){
+    return true;
+  }
+  return false;
+}
+
+function startsWithD(letter){
+  if(letter.charAt[0] = 'D'){
+    return true;
+  }
+  return false;
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -6,8 +6,20 @@
   If Daniel is a student, print out "Hi, I'm Daniel, I'm a student."
 */
 
+// let name = "Daniel";
+// let danielsRole = "mentor";
+function role(name, danielsRole){
+  if((danielsRole = "mentor") && (name= "Daniel")){
+    console.log("Hi, I'm Daniel, I'm a mentor.");
+  }
+  else {
+    console.log("Hi, I'm Daniel, I'm a student.");
+  }
+}
 let name = "Daniel";
 let danielsRole = "mentor";
+role();
+
 
 /* 
 EXPECTED RESULT
