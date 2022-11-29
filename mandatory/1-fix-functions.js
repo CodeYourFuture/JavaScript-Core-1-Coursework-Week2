5eafe10b169955034d5f531f59e9a76836f7e182
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2022-11-29 19:34:15 +0000

    Mandatory done

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,29 +10,25 @@
 
 */
 
-function mood() {
-  let isHappy = true;
-
+function mood(isHappy) {
   if (isHappy) {
     return "I am happy";
-  } else {
-    return "I am not happy";
   }
+  return "I am not happy";
 }
 
 function greaterThan10(num) {
-  let isBigEnough;
+  let isBigEnough = num > 10;
 
   if (isBigEnough) {
     return "num is greater than 10";
-  } else {
-    return "num is not big enough";
   }
+  return "num is not big enough";
 }
 
 function get3rdIndex(arr) {
   let index = 3;
-  let element;
+  let element = arr[index];
 
   return element;
 }
