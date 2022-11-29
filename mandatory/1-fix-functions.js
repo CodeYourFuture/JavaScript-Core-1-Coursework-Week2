ff93354456e36a79661f11a75cb2ad4a351c2657
Zobeir <rigi.zobir@yahoo.com>
2022-11-29 14:13:12 +0000

    mandatory

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,8 +10,10 @@
 
 */
 
-function mood() {
+
   let isHappy = true;
+function mood(isHappy) {
+  
 
   if (isHappy) {
     return "I am happy";
@@ -21,9 +23,10 @@ function mood() {
 }
 
 function greaterThan10(num) {
-  let isBigEnough;
+  let isBigEnough = num ;
+ 
 
-  if (isBigEnough) {
+  if (isBigEnough > 10) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
@@ -32,7 +35,7 @@ function greaterThan10(num) {
 
 function get3rdIndex(arr) {
   let index = 3;
-  let element;
+  let element = arr [index];
 
   return element;
 }
