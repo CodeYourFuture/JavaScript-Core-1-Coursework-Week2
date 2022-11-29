e079a0040e20c8afbeba0080838ddb4fd5ef5f7b
Zobeir <rigi.zobir@yahoo.com>
2022-11-29 17:43:43 +0000

    mandatory

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,9 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  return (typeof num === "number") && (num % 2 == 0 ) && (num <= 100) ;
+}
 
 /*
 Write a function that:
@@ -16,7 +18,10 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+    
+      (num > 100)?   num.replace("100")   : `% ${num.toFixed(2)} `;
+}
 
 /*
 Write a function that:
@@ -25,7 +30,11 @@ Write a function that:
 - removes any forward slashes (/) in each string
 - makes all strings all lowercase
 */
-function tidyUpStrings(arrayOfStrings) {}
+function tidyUpStrings(arrayOfStrings) {
+     return arrayOfStrings.trim().replace("/","") .toLowercase() ; 
+      
+      
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
