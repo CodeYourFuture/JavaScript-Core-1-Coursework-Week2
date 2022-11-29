d8497da4a3afe8a93da6cdc56dcf3dde2900a0ba
Abdulmajidram <115878433+Abdulmajidram@users.noreply.github.com>
2022-11-29 12:48:54 +0000

    Mandatory
    managed to do two of the exercises and one didn't complete it fully , well edit it once I know the solution.

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -6,7 +6,13 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+   if (typeof num === "number" && num % 2 === 0 && num <= 100) {
+    return true;
+  }
+  return false;
+}
+
 
 /*
 Write a function that:
@@ -16,7 +22,9 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(num) {}
+function formatPercentage(num) {
+  
+}
 
 /*
 Write a function that:
