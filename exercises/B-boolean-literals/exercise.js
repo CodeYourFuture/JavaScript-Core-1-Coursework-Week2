4e7c410055cb99aff220fdd343eb6d7e70cd19ac
Sana Asaf <sanaasaf@gmail.com>
2022-11-30 19:42:53 +0000

    Exercises and mandatory completed
    Working on the extra exercise.

--- exercises/B-boolean-literals/exercise.js
+++ exercises/B-boolean-literals/exercise.js
@@ -6,6 +6,9 @@
 */
 
 let codeYourFutureIsGreat = true;
+let mozafarIsCool= true;
+let calculationCorrect= true;
+let moreThan10Students= true;
 
 /* 
   DO NOT EDIT BELOW THIS LINE
--- mandatory/3-playing-computer.js
+++ mandatory/3-playing-computer.js
@@ -6,13 +6,26 @@
 
   Answer the following questions:
 
-  1. This program throws an error. Why? (If you can't find it, try executing it).
+  1. This program throws an error. Why? (If you can't find it, try executing it). 
+     b is not defined.
   2. Remove the line that throws the error.
+    line32 commented
   3. What is printed to the console?
+2
+6
+4
+9
+6
+13
+8
   4. How many times is "f1" called?
+  2 times
   5. How many times is "f2" called?
+  3 times
   6. What value does the "a" parameter take in the first "f1" call?
+  1
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
+  8
 */
 
 let x = 2;
@@ -28,7 +41,7 @@ const f2 = function (a, b) {
 
 console.log(x);
 console.log(a);
-console.log(b);
+//console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
@@ -36,6 +49,7 @@ for (let i = 0; i < 5; ++i) {
     const d = f2(i, x);
     console.log(d);
   } else {
+  
     const e = f1(i, a);
     console.log(e);
   }
