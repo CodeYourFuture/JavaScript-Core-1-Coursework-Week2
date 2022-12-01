753489cd8cc603aedde18513c1f8ebf31cfe9527
Natalie Zablotska <113237479+nataliiazab@users.noreply.github.com>
2022-12-01 17:55:13 +0000

    complete exercises J

--- exercises/J-array-get-set/exercise.js
+++ exercises/J-array-get-set/exercise.js
@@ -5,11 +5,12 @@
 */
 
 function first(arr) {
-  return; // complete this statement
+  return arr[0]; // complete this statement
 }
 
 function last(arr) {
-  return; // complete this statement
+  let lastIndex = arr.length - 1;
+  return arr[lastIndex]; // complete this statement
 }
 
 /* 
