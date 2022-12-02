bcad85f909dec2cc470cc7c096aa189cc425a3e4
ali1373107 <alidashti1994a@gmail.com>
2022-12-02 20:10:52 +0000

    first commit

--- exercises/D-logical-operators/exercise2.js
+++ exercises/D-logical-operators/exercise2.js
@@ -5,7 +5,21 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
+function isNegative(num) {
+  return num < 0;
+}
+
+function isBetween5and10(num) {
+  return num > 5 && num < 10;
+}
+
+function isShortName(str) {
+  return str.split(" ").length === 1;
+}
+
+function startsWithD(str) {
+  return str[0] === "D";
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
