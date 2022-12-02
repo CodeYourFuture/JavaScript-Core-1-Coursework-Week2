ab2d1410a43ea454a5bdc035b2e07f11e0bc0387
AndrianaOS <andrianasaffo@gmail.com>
2022-12-02 09:00:16 +0000

    Included a function and variable

--- extra/1-radio-stations.js
+++ extra/1-radio-stations.js
@@ -14,6 +14,14 @@
  */
 
 // `getAllFrequencies` goes here
+function getAllFrequencies() {
+  return listOfFrequencies;
+}
+
+const listOfFrequencies = [
+  87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104,
+  105, 106, 107, 108,
+];
 
 /**
  * Next, let's write a function that gives us only the frequencies that are radio stations.
