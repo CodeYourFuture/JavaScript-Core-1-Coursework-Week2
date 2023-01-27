0773e5b2d27598cda00d930af6186493d9b911f8
dubchoi <will@jomolabs.co>
2023-01-27 10:17:24 -0800

    Swap exercises w/ week3 to match the syllabus. Make sure loops are here in week2, and arrays are in week3.

---
+++ exercises/J-for-of-loop/exercise.js
@@ -0,0 +1,6 @@
+/*
+    A for-of loop is a easy and way of looping through the elements of an array, string or any other "iterable object" (think sequence of elements).    
+*/
+
+// TODO Use a for-of loop to capitalise and output each letter in the string seperately.
+let str = "codeyourfuture";
