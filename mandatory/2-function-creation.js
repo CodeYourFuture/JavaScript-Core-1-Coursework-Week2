82db6f7685fbf2f00f5352a5bbc59992bceff138
mandy-cheung <109825538+m4ndycheung@users.noreply.github.com>
2023-02-24 19:55:15 +0000

    Completed 1-fix-functions, added to 2-function-creation

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -4,7 +4,13 @@
     1. the user should be 18 or older
     2. the user must be logged in
 */
-function isAcceptableUser(userAge, isLoggedIn) {}
+function isAcceptableUser(userAge, isLoggedIn) {
+  if (userAge >=18 && isLoggedIn === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /*
   Complete the function to apply discount percent based on how much is totalPrice in user cart.
