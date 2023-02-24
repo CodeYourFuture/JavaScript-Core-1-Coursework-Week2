4f33153ee6fb954ff4021a7946017f2f34d56647
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-02-24 00:41:49 +0000

    Update 2-function-creation.js

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -23,19 +23,37 @@ function isAcceptableUser(userAge, isLoggedIn) {
 */
 
 function applyDiscount(totalPrice) {
-  let priceAfterDiscount = 0.95
+  if (totalPrice > 200){
+  return totalPrice * 0.9;
+} else {
+  return totalPrice * 0.95;}
+
 }
 
 /*
   Complete the function to print to the console the odd numbers between 1 and limit (use a while loop):
   */
-function printOddNumbers(limit) {}
+function printOddNumbers(limit) {
+  let i = 1;
+  while (i <= limit) {
+    if ( i % 2 == 1) {
+      console.log(i);
+    }
+    i++;
+    }
+  }
 
 /*
   Complete the buyTwoGetTheCheapestFree function: if user buys two items, the cheapest item will be free!
   The function should return the price to be paid once the discount is applied.
 */
-function buyTwoGetTheCheapestFree(price1, price2) {}
+function buyTwoGetTheCheapestFree(price1, price2) {
+  if (price1 <= price2) {
+    return price2;
+  }else {
+    return price1;
+  }
+}
 
 /*
   Complete the function to determine if it is suitable for a person to register based on their age!
@@ -43,7 +61,15 @@ function buyTwoGetTheCheapestFree(price1, price2) {}
   - if the person is older than 12 and younger than 90 it should return "You Can Register"
   - if the person is 90 or older it should return "You Don't Need To Register"
 */
-function canRegister(age) {}
+function canRegister(age) {
+  if (age <= 12) {
+    return "You Are Too Young To Register";
+ } else  if (age >= 90) {
+  return "You Don't Need To Register";
+ } else {
+  return "You Can Register";
+ }
+}
 
 /*
   Complete the function so that it prints out to the console numbers in reverse order starting at
@@ -54,7 +80,13 @@ function canRegister(age) {}
   )
 */
 
-function countReverse(number) {}
+function countReverse(number) {
+  while (number >= 1){
+    console.log(number);
+    number--;
+    }
+  }
+
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
