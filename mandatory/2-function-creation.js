11a98caa69c06b267ef7dc55bbc1d4d7e606488a
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-02-26 22:21:30 +0000

    Update 2-function-creation.js
    Finished all the mandatory exercises

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -58,7 +58,14 @@ function printOddNumbers(limit) {
   Complete the buyTwoGetTheCheapestFree function: if user buys two items, the cheapest item will be free!
   The function should return the price to be paid once the discount is applied.
 */
-function buyTwoGetTheCheapestFree(price1, price2) {}
+function buyTwoGetTheCheapestFree(price1, price2) {
+  if (price1 > price2){
+    return price1;
+  }
+  else {
+    return price2;
+  }
+}
 
 /*
   Complete the function to determine if it is suitable for a person to register based on their age!
@@ -66,7 +73,18 @@ function buyTwoGetTheCheapestFree(price1, price2) {}
   - if the person is older than 12 and younger than 90 it should return "You Can Register"
   - if the person is 90 or older it should return "You Don't Need To Register"
 */
-function canRegister(age) {}
+function canRegister(age) {
+  if (age <= 12) {
+    return "You Are Too Young To Register";
+  }
+  else if ( age > 12 && age < 90){
+    return "You Can Register";
+  }
+  else {
+    return "You Don't Need To Register"
+  }    
+    
+}
 
 /*
   Complete the function so that it prints out to the console numbers in reverse order starting at
@@ -77,7 +95,14 @@ function canRegister(age) {}
   )
 */
 
-function countReverse(number) {}
+function countReverse(number) {
+  while (number > 0) {
+    console.log(number);
+    number -- ;
+
+  }
+
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
