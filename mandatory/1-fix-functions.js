24487d7fd39b4900e66b5caabde1eb9b158c4fb4
Heresh <108929624+HereshT@users.noreply.github.com>
2023-03-01 10:46:19 +0000

    Glasgow_6 - HERISH  TURKI - JS -Core-1-Coursework-Week2
    Glasgow_6 - HERISH  TURKI - JS -Core-1-Coursework-Week2

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,20 +10,18 @@
 
 */
 
-function getMood() {
-  let isHappy = true;
-
-  if (isHappy) {
+function getMood(isHappy) {
+  if (isHappy === true) {
     return "I am happy";
   } else {
     return "I am not happy";
   }
 }
 
-function greaterThan10(num) {
-  let isBigEnough;
+////////////////////
 
-  if (isBigEnough) {
+function greaterThan10(num) {
+  if (num > 10) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
