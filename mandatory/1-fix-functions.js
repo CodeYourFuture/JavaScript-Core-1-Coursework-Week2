3bbd93f8ddb38d2d39da047f040d26d6867d0027
CYF <cyf@CYFs-MacBook-Pro.local>
2023-03-04 16:03:31 +0000

    final change

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,20 +10,17 @@
 
 */
 
-function getMood() {
-  let isHappy = true;
-
-  if (isHappy) {
+function getMood(happy) {
+  if (happy == true) {
     return "I am happy";
-  } else {
+  } else if (happy == false){
     return "I am not happy";
   }
 }
 
 function greaterThan10(num) {
-  let isBigEnough;
 
-  if (isBigEnough) {
+  if (num>10) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
