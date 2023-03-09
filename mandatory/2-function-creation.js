3ca5ff9dab35f21018b0664e6ef42d23bb6cb05a
Ehdaa Munier <109866621+Ehdaa-Munier@users.noreply.github.com>
2023-03-09 11:52:57 +0000

    fix mandatory

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -45,15 +45,35 @@ function printOddNumbers(limit) {
   Complete the buyTwoGetTheCheapestFree function: if user buys two items, the cheapest item will be free!
   The function should return the price to be paid once the discount is applied.
 */
-function buyTwoGetTheCheapestFree(price1, price2) {}
+function buyTwoGetTheCheapestFree(price1, price2) {
+  let totalPrice = price1 + price2;
+  if (price1 < price2){
+    let discount = price1;
+  }
+  return  totalPrice - price1;
+  }
 
+  
 /*
   Complete the function to determine if it is suitable for a person to register based on their age!
   - if the person is 12 or younger it should return "You Are Too Young To Register"
   - if the person is older than 12 and younger than 90 it should return "You Can Register"
   - if the person is 90 or older it should return "You Don't Need To Register"
 */
-function canRegister(age) {}
+function canRegister(age) {
+  if (age <= 12){
+    return "You Are Too Young To Register"
+  }
+   else if (age > 12 && age < 90) {
+    return "You Can Register"
+   }
+   else {
+    return "You Don't Need To Register"
+   }
+
+  
+
+}
 
 /*
   Complete the function so that it prints out to the console numbers in reverse order starting at
