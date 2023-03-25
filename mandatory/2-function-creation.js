be72802a4d310358e9fb729cdaf00204cd877cdd
onurat <114289826+onurat@users.noreply.github.com>
2023-03-25 12:49:33 +0000

    js-1-week2-onur-atas

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -37,35 +37,50 @@ function applyDiscount(totalPrice) {
 }
 
 
-/*
+/*let limit = 0;
   Complete the function to print to the console the odd numbers between 1 and limit (use a while loop):
   */
 function printOddNumbers(limit) {
-  let limit = 0;
 
-  for (let i = 0; i > limit; i++) {
+  for (let i = 0; i <= limit; i++) {
     if (i % 2 === 1) {
-      return i;
+      console.log(i);
     }
   }
 
 
-} let limit = 0; let limit = 0;
+}
 
 /*
   Complete the buyTwoGetTheCheapestFree function: if user buys two items, the cheapest item will be free!
   The function should return the price to be paid once the discount is applied.
 */
-function buyTwoGetTheCheapestFree(price1, price2) { }
+function buyTwoGetTheCheapestFree(price1, price2) {
+  if (price1 > price2) {
+    return price1;
+  }
+  else {
+    return price2;
 
+  }
+
+
+}
 /*
   Complete the function to determine if it is suitable for a person to register based on their age!
   - if the person is 12 or younger it should return "You Are Too Young To Register"
   - if the person is older than 12 and younger than 90 it should return "You Can Register"
   - if the person is 90 or older it should return "You Don't Need To Register"
 */
-function canRegister(age) { }
-
+function canRegister(age) {
+  if (age <= 12) {
+    return "You Are Too Young To Register";
+  } else if (age < 90) {
+    return "You Can Register";
+  } else {
+    return "You Don't Need To Register";
+  }
+}
 /*
   Complete the function so that it prints out to the console numbers in reverse order starting at
   number and going down to 1 (e.g. if number was 3, it would print:
@@ -75,7 +90,11 @@ function canRegister(age) { }
   )
 */
 
-function countReverse(number) { }
+function countReverse(number) {
+  for (let i = number; i >= 1; i--) {
+    console.log(i);
+  }
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
