5efee4961d14b671532c1142cd31d0aa533ce319
Yuliya110692 <122671093+Yuliya110692@users.noreply.github.com>
2023-03-25 14:48:34 +0000

    Update 2-function-creation.js

--- mandatory/2-function-creation.js
+++ mandatory/2-function-creation.js
@@ -4,7 +4,12 @@
     1. the user should be 18 or older
     2. the user must be logged in
 */
-function isAcceptableUser(userAge, isLoggedIn) {}
+function isAcceptableUser(userAge, isLoggedIn) {
+
+  
+
+
+}
 
 /*
   Complete the function to apply discount percent based on how much is totalPrice in user cart.
@@ -15,7 +20,9 @@ function isAcceptableUser(userAge, isLoggedIn) {}
     is applieds and 142.5 should be returned)
 */
 
-function applyDiscount(totalPrice) {}
+function applyDiscount(totalPrice) {
+  
+}
 
 /*
   Complete the function to print to the console the odd numbers between 1 and limit (use a while loop):
